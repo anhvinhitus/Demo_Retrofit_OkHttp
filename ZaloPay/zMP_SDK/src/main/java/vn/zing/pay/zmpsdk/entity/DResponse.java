de25f2c3054545c7d9d235157d90262a7a9e9da9
Long le van <levanlong318@gmail.com>
2016-05-10 11:09:28 +0700

    remove zaloPaymentSDK

--- ZaloPay/zMP_SDK/src/main/java/vn/zing/pay/zmpsdk/entity/DResponse.java
+++
@@ -1,7 +0,0 @@
-package vn.zing.pay.zmpsdk.entity;
-
-public class DResponse extends DBaseEntity<DResponse> {
-	public int returnCode = 0;
-	public String returnMessage = null;
-	public String zmpTransID;
-}
