8f637373333c57b17bba64b2ab7dbbd1480d86b2
Hiếu Vũ Mạnh <manhhieu1412@gmail.com>
2016-05-15 07:32:15 +0700

    fix bug: Scan qr trong khung hình.

---
+++ ZaloPay/qrcodecore/src/main/java/vn/com/vng/zalopay/qrcode/CustomViewfinderView.java
@@ -0,0 +1,98 @@
+package vn.com.vng.zalopay.qrcode;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+
+import com.journeyapps.barcodescanner.ViewfinderView;
+
+/**
+ * Created by AnhHieu on 5/14/16.
+ */
+public class CustomViewfinderView extends ViewfinderView {
+
+    private static final int ANIMATION_DELAY = 1;
+    private int SPEEN_DISTANCE = 5;
+
+    private Rect mFramingRect;
+    private int mScanLineTop;
+    private Paint mPaint;
+    private int mMaskColor;
+    private int mCornerColor;
+    private int mCornerLength;
+    private int mCornerSize;
+    private int mRectWidth;
+    private int mTopOffset;
+    private int mScanLineSize;
+    private int mScanLineColor;
+
+    public CustomViewfinderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+        mMaskColor = Color.parseColor("#33ffffff");
+        mCornerColor =  Color.parseColor("#018fe5");
+        mCornerLength = DisplayUtils.dp2px(context, 20);
+        mCornerSize = DisplayUtils.dp2px(context, 2);
+        mScanLineSize = DisplayUtils.dp2px(context, 1);
+        mScanLineColor =  Color.parseColor("#018fe5");
+        mTopOffset = DisplayUtils.dp2px(context, 0);
+        mRectWidth = context.getResources().getDimensionPixelOffset(R.dimen.rect_scan_size);
+    }
+
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        if (mFramingRect == null) {
+            return;
+        }
+
+        // 获取屏幕的宽和高
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        mPaint.setColor(mMaskColor);
+        canvas.drawRect(0, 0, width, mFramingRect.top, mPaint);
+        canvas.drawRect(0, mFramingRect.top, mFramingRect.left, mFramingRect.bottom + 1, mPaint);
+        canvas.drawRect(mFramingRect.right + 1, mFramingRect.top, width, mFramingRect.bottom + 1, mPaint);
+        canvas.drawRect(0, mFramingRect.bottom + 1, width, height, mPaint);
+
+
+        mPaint.setColor(mCornerColor);
+        canvas.drawRect(mFramingRect.left - mCornerSize + 2, mFramingRect.top - mCornerSize + 2, mFramingRect.left + mCornerLength - mCornerSize + 2, mFramingRect.top + 2, mPaint);
+        canvas.drawRect(mFramingRect.left - mCornerSize + 2, mFramingRect.top - mCornerSize + 2, mFramingRect.left + 2, mFramingRect.top + mCornerLength - mCornerSize + 2, mPaint);
+        canvas.drawRect(mFramingRect.right - mCornerLength + mCornerSize - 2, mFramingRect.top - mCornerSize + 2, mFramingRect.right + mCornerSize - 2, mFramingRect.top + 2, mPaint);
+        canvas.drawRect(mFramingRect.right - 2, mFramingRect.top - mCornerSize + 2, mFramingRect.right + mCornerSize - 2, mFramingRect.top + mCornerLength - mCornerSize + 2, mPaint);
+
+        canvas.drawRect(mFramingRect.left - mCornerSize + 2, mFramingRect.bottom - 2, mFramingRect.left + mCornerLength - mCornerSize + 2, mFramingRect.bottom + mCornerSize - 2, mPaint);
+        canvas.drawRect(mFramingRect.left - mCornerSize + 2, mFramingRect.bottom - mCornerLength + mCornerSize - 2, mFramingRect.left + 2, mFramingRect.bottom + mCornerSize - 2, mPaint);
+        canvas.drawRect(mFramingRect.right - mCornerLength + mCornerSize - 2, mFramingRect.bottom - 2, mFramingRect.right + mCornerSize - 2, mFramingRect.bottom + mCornerSize - 2, mPaint);
+        canvas.drawRect(mFramingRect.right - 2, mFramingRect.bottom - mCornerLength + mCornerSize - 2, mFramingRect.right + mCornerSize - 2, mFramingRect.bottom + mCornerSize - 2, mPaint);
+
+
+        mPaint.setColor(mScanLineColor);
+        canvas.drawRect(mFramingRect.left, mScanLineTop, mFramingRect.right, mScanLineTop + mScanLineSize, mPaint);
+        mScanLineTop += SPEEN_DISTANCE;
+        if (mScanLineTop >= mFramingRect.bottom || mScanLineTop <= mFramingRect.top) {
+            SPEEN_DISTANCE = -SPEEN_DISTANCE;
+        }
+
+        postInvalidateDelayed(ANIMATION_DELAY, mFramingRect.left, mFramingRect.top, mFramingRect.right, mFramingRect.bottom);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Point screenResolution = DisplayUtils.getScreenResolution(getContext());
+        int leftOffset = (screenResolution.x - mRectWidth) / 2;
+        mTopOffset = (h - mRectWidth) / 2;
+        mFramingRect = new Rect(leftOffset, mTopOffset, leftOffset + mRectWidth, mTopOffset + mRectWidth);
+        mScanLineTop = mTopOffset;
+    }
+
+}
---
+++ ZaloPay/qrcodecore/src/main/java/vn/com/vng/zalopay/qrcode/activity/AbsQRScanActivity.java
@@ -0,0 +1,112 @@
+package vn.com.vng.zalopay.qrcode.activity;
+
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
+import java.util.List;
+
+import vn.com.vng.zalopay.qrcode.BuildConfig;
+import vn.com.vng.zalopay.qrcode.R;
+
+/**
+ * Created by AnhHieu on 5/14/16.
+ */
+public abstract class AbsQRScanActivity extends AppCompatActivity {
+
+    protected abstract void handleResult(String result);
+
+    protected final String TAG = this.getClass().getSimpleName();
+
+    private CompoundBarcodeView barcodeScannerView;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() != null) {
+                if (BuildConfig.DEBUG) {
+                    barcodeScannerView.setStatusText(result.getText());
+                }
+                barcodeScannerView.pause();
+                handleResult(result.getText());
+            }
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getResLayoutId());
+        barcodeScannerView = (CompoundBarcodeView) findViewById(R.id.zxing_barcode_scanner);
+        barcodeScannerView.decodeContinuous(callback);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (barcodeScannerView != null) {
+            barcodeScannerView.resume();
+        }
+
+    }
+
+    public int getResLayoutId() {
+        return R.layout.capture_appcompat;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        barcodeScannerView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        barcodeScannerView.pause();
+    }
+
+    public void triggerScan(View view) {
+        barcodeScannerView.decodeSingle(callback);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    protected void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
+    }
+
+
+}
