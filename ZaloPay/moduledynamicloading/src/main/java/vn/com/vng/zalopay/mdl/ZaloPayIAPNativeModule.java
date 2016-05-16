518eac222aac3ef8911de42b1a5b123066f724e8
Long le van <levanlong318@gmail.com>
2016-05-16 12:31:59 +0700

    inprogress: add pay method

--- ZaloPay/moduledynamicloading/src/main/java/vn/com/vng/zalopay/mdl/ZaloPayIAPNativeModule.java
+++ ZaloPay/moduledynamicloading/src/main/java/vn/com/vng/zalopay/mdl/ZaloPayIAPNativeModule.java
@@ -6,6 +6,14 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 
+import timber.log.Timber;
+import vn.zing.pay.zmpsdk.ZingMobilePayService;
+import vn.zing.pay.zmpsdk.entity.ZPPaymentResult;
+import vn.zing.pay.zmpsdk.entity.ZPWPaymentInfo;
+import vn.zing.pay.zmpsdk.entity.enumeration.EPaymentChannel;
+import vn.zing.pay.zmpsdk.entity.enumeration.EPaymentStatus;
+import vn.zing.pay.zmpsdk.listener.ZPPaymentListener;
+
 /**
  * Created by huuhoa on 5/16/16.
  */
@@ -27,9 +35,53 @@ public class ZaloPayIAPNativeModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void payOrder(ReadableMap params, Promise promise) {
         // verify params parameters
+        params.getString()
+
 
         // call payment SDK
+        ZPWPaymentInfo paymentInfo = new ZPWPaymentInfo();
+        EPaymentChannel forcedPaymentChannel = EPaymentChannel.LINK_CARD;
+        paymentInfo.zaloUserID = String.valueOf(user.uid);
+        paymentInfo.zaloPayAccessToken = user.accesstoken;
+
+        ZingMobilePayService.pay(mView.getActivity(), forcedPaymentChannel, paymentInfo, zpPaymentListener);
 
         // return result
+//        zpPaymentListener
     }
+
+    ZPPaymentListener zpPaymentListener = new ZPPaymentListener() {
+        @Override
+        public void onComplete(ZPPaymentResult zpPaymentResult) {
+            if (zpPaymentResult == null) {
+//                if (!AndroidUtils.isNetworkAvailable(mView.getContext())) {
+//                    mView.showError("Vui lòng kiểm tra kết nối mạng và thử lại.");
+//                } else {
+//                    mView.showError("Lỗi xảy ra trong quá trình liên kết thẻ. Vui lòng thử lại sau.");
+//                }
+            } else {
+                EPaymentStatus paymentStatus = zpPaymentResult.paymentStatus;
+                if (paymentStatus.getNum() == EPaymentStatus.ZPC_TRANXSTATUS_SUCCESS.getNum()) {
+//                    getBalance();
+//                    ZPWPaymentInfo paymentInfo = zpPaymentResult.paymentInfo;
+//                    if (paymentInfo == null) {
+//                        return;
+//                    }
+//                    mView.onAddCardSuccess(paymentInfo.mappedCreditCard);
+                } else if (paymentStatus.getNum() == EPaymentStatus.ZPC_TRANXSTATUS_TOKEN_INVALID.getNum()) {
+//                    mView.onTokenInvalid();
+                }
+            }
+        }
+
+        @Override
+        public void onCancel() {
+//            hideLoadingView();
+        }
+
+        @Override
+        public void onSMSCallBack(String s) {
+
+        }
+    };
 }
