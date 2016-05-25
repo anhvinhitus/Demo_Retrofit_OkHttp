b2a1b694f9f104386eca753c13b07660f542e6cf
Hiếu Vũ Mạnh <manhhieu1412@gmail.com>
2016-05-25 17:42:47 +0700

    clean code

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/fragment/tabmain/ZaloPayFragment.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/fragment/tabmain/ZaloPayFragment.java
@@ -1,14 +1,10 @@
 package vn.com.vng.zalopay.ui.fragment.tabmain;
 
 import android.Manifest;
-import android.content.Intent;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.text.TextUtils;
@@ -25,16 +21,13 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.OnClick;
-import timber.log.Timber;
-import vn.com.vng.zalopay.AndroidApplication;
 import vn.com.vng.zalopay.R;
-import vn.com.vng.zalopay.data.api.response.PlatformInfoResponse;
 import vn.com.vng.zalopay.domain.model.AppResource;
 import vn.com.vng.zalopay.domain.model.User;
 import vn.com.vng.zalopay.navigation.Navigator;
-import vn.com.vng.zalopay.paymentapps.ui.PaymentApplicationActivity;
 import vn.com.vng.zalopay.ui.adapter.BannerPagerAdapter;
 import vn.com.vng.zalopay.ui.adapter.ListAppRecyclerAdapter;
+import vn.com.vng.zalopay.ui.widget.GridSpacingItemDecoration;
 
 /**
  * Created by AnhHieu on 4/11/16.
@@ -101,11 +94,7 @@ public class ZaloPayFragment extends BaseMainFragment implements ListAppRecycler
         listView.setHasFixedSize(true);
         listView.setLayoutManager(new GridLayoutManager(getContext(), 3));
         listView.setNestedScrollingEnabled(false);
-        int spanCount = 3; // 3 columns
-        int spacing = 2; // 50px
-        boolean includeEdge = false;
-
-        listView.addItemDecoration(new GridSpacingItemDecoration(spanCount, spacing, includeEdge));
+        listView.addItemDecoration(new GridSpacingItemDecoration(3, 2, false));
 
         listView.setAdapter(mAdapter);
 
@@ -183,39 +172,5 @@ public class ZaloPayFragment extends BaseMainFragment implements ListAppRecycler
                 new AppResource(6, getString(R.string.water_bill), String.valueOf(R.drawable.ic_tiennuoc))
         );
 
-    }
-
-    public static class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
-                }
-            }
-        }
     }
 }
