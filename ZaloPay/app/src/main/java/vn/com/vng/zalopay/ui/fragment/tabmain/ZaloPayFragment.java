c34c0ad1322532a1ff1f7f725ea2ac7e7b371ce3
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-06-03 17:11:49 +0700

    Update app 11,12

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/fragment/tabmain/ZaloPayFragment.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/fragment/tabmain/ZaloPayFragment.java
@@ -180,9 +180,9 @@ public class ZaloPayFragment extends BaseMainFragment implements ListAppRecycler
     //Test
     private List<AppResource> getListData() {
         return Arrays.asList(new AppResource(1, getString(R.string.transfer_money), String.valueOf(R.drawable.ic_chuyentien)),
-                new AppResource(2, getString(R.string.recharge_money_phone), String.valueOf(R.drawable.ic_naptiendt)),
-                new AppResource(11, getString(R.string.buy_phone_card), String.valueOf(R.drawable.ic_muathedt)),
-                new AppResource(12, getString(R.string.buy_game_card), String.valueOf(R.drawable.ic_muathegame)),
+                new AppResource(11, getString(R.string.recharge_money_phone), String.valueOf(R.drawable.ic_naptiendt)),
+                new AppResource(12, getString(R.string.buy_phone_card), String.valueOf(R.drawable.ic_muathedt)),
+                new AppResource(13, getString(R.string.buy_game_card), String.valueOf(R.drawable.ic_muathegame)),
                 new AppResource(3, getString(R.string.electric_bill), String.valueOf(R.drawable.ic_tiendien)),
                 new AppResource(4, getString(R.string.internet_bill), String.valueOf(R.drawable.ic_internet)),
                 new AppResource(5, getString(R.string.red_envelope), String.valueOf(R.drawable.ic_lixi)),
