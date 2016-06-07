75cbd556a911de26f20679780b437dc9d81cbdab
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-06-07 13:30:35 +0700

    Cleanup

--- ZaloPay/sounddecoder/src/main/java/vn/com/vng/zalopay/crity/CrityWrapper.java
+++ ZaloPay/sounddecoder/src/main/java/vn/com/vng/zalopay/crity/CrityWrapper.java
@@ -7,6 +7,7 @@ import vn.com.vng.grd.crity.BuildConfig;
 
 /**
  * Created by hanv2 on 9/24/15.
+ *
  */
 public class CrityWrapper {
     // Declare native method (and make it public to expose it directly)
@@ -23,12 +24,6 @@ public class CrityWrapper {
             System.loadLibrary("crity");
         } catch (Throwable ex) {
             Log.e("Wrapper", "Error loading crity", ex);
-            try {
-                System.load("/data/data/vn.com.vng.zalopay/lib/libcrity.so");
-            } catch (Throwable ex2) {
-                ex2.printStackTrace();
-                Log.e("SecurityWrapper", "Cannot load native libs", ex2);
-            }
         }
     }
 }
