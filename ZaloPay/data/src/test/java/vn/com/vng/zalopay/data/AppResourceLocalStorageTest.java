37238294039f3418f6131479b058d69c62decb29
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-06-20 14:10:13 +0700

    Refactor AppResourceLocalStorageTest

--- ZaloPay/data/src/test/java/vn/com/vng/zalopay/data/AppResourceLocalStorageTest.java
+++ ZaloPay/data/src/test/java/vn/com/vng/zalopay/data/AppResourceLocalStorageTest.java
@@ -31,14 +31,28 @@ import static org.junit.Assert.assertTrue;
 @Config(constants = BuildConfig.class, sdk = 16)
 public class AppResourceLocalStorageTest {
     private AppResourceLocalStorage mLocalStorage;
-    private DaoSession mDaoSession;
 
     @Before
     public void setUp() throws Exception {
         DaoMaster.DevOpenHelper openHelper = new DaoMaster.DevOpenHelper(RuntimeEnvironment.application, null, null);
         SQLiteDatabase db = openHelper.getWritableDatabase();
-        mDaoSession = new DaoMaster(db).newSession();
-        mLocalStorage = new AppResourceLocalStorage(mDaoSession, new PlatformDaoMapper());
+        DaoSession daoSession = new DaoMaster(db).newSession();
+        mLocalStorage = new AppResourceLocalStorage(daoSession, new PlatformDaoMapper());
+    }
+
+    private AppResourceEntity createAppResourceEntity(int appid) {
+        AppResourceEntity entity = new AppResourceEntity();
+        entity.appid = appid;
+        entity.appname = "Nạp tiền ĐT";
+        entity.checksum = "11";
+        entity.imageurl = "imageurl";
+        entity.jsurl = "jsurl";
+        entity.needdownloadrs = 1;
+        entity.numRetry = 0;
+        entity.status = 1;
+        entity.timeDownload = 0;
+        entity.stateDownload = 0;
+        return entity;
     }
 
     @Test
@@ -58,21 +72,6 @@ public class AppResourceLocalStorageTest {
         List<AppResourceEntity> result = mLocalStorage.get();
 
         assertTrue(resourceEntityList.equals(result));
-    }
-
-    AppResourceEntity createAppResourceEntity(int appid) {
-        AppResourceEntity entity = new AppResourceEntity();
-        entity.appid = appid;
-        entity.appname = "Nạp tiền ĐT";
-        entity.checksum = "11";
-        entity.imageurl = "imageurl";
-        entity.jsurl = "jsurl";
-        entity.needdownloadrs = 1;
-        entity.numRetry = 0;
-        entity.status = 1;
-        entity.timeDownload = 0;
-        entity.stateDownload = 0;
-        return entity;
     }
 
     @Test
