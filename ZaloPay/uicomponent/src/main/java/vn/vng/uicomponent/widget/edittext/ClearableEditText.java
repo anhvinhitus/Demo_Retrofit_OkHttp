4d09b3431109efb2981aa5aa54affbfb0150cb84
Long le van <levanlong318@gmail.com>
2016-07-03 17:23:35 +0700

    update ClearableEditText

--- ZaloPay/uicomponent/src/main/java/vn/vng/uicomponent/widget/edittext/ClearableEditText.java
+++ ZaloPay/uicomponent/src/main/java/vn/vng/uicomponent/widget/edittext/ClearableEditText.java
@@ -1,108 +1,144 @@
 package vn.vng.uicomponent.widget.edittext;
 
+
 import android.content.Context;
-import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.v7.widget.AppCompatEditText;
+import android.os.Build;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
 import vn.vng.uicomponent.widget.R;
 
 /**
- * Created by longlv on 26/04/2016.
+ * <p/>
+ * Created by JAY on 2015/4/18.
  */
-public class ClearableEditText extends AppCompatEditText {
-    private static final String TAG = "ClearableTextView";
+public class ClearableEditText extends EditText implements View.OnTouchListener,
+        View.OnFocusChangeListener, TextWatcher {
+
+
+    public interface ClearListener {
+        void didClearText();
+    }
+
+    public void setClearListener(ClearListener clearListener) {
+        this.clearListener = clearListener;
+    }
+
+    private Drawable clearDrawable;
+    private ClearListener clearListener;
+
+    public ClearableEditText(Context context) {
+        super(context);
+        init();
+    }
 
     public ClearableEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-    }
-    public ClearableEditText(Context context) {
-        super(context);
-        // TODO Auto-generated constructor stub
+        init();
     }
 
     public ClearableEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        // TODO Auto-generated constructor stub
+        init();
     }
-//	private void hideKeyboard() {
-//		if (getContext() == null)
-//			return;
-////		if (view == null)
-////			return;
-//		InputMethodManager manager = (InputMethodManager) getContext()
-//				.getSystemService(Context.INPUT_METHOD_SERVICE);
-//		manager.hideSoftInputFromWindow(this.getWindowToken(), 0);
-//	}
 
-    //support clear icon
-    private Drawable dRight;
-    private Rect rBounds;
-    private boolean mClearable;
     @Override
-    public void setCompoundDrawables(Drawable left, Drawable top,
-                                     Drawable right, Drawable bottom) {
-        // TODO Auto-generated method stub
-//		if (right != null) {
-        dRight = right;
-//		}
-//		dRight.setVisible(isEnabled(), true);
-        super.setCompoundDrawables(left, top, right, bottom);
+    public void setOnTouchListener(OnTouchListener l) {
+        this.l = l;
     }
 
-    public boolean isClearable(){
-        return mClearable;
+    @Override
+    public void setOnFocusChangeListener(OnFocusChangeListener f) {
+        this.f = f;
     }
-    public void setClearable(boolean clearable){
-        mClearable = clearable;
-        Drawable[] compoundDrawables = getCompoundDrawables();
-        if (mClearable){
-            setCompoundDrawablesWithIntrinsicBounds(compoundDrawables[0], compoundDrawables[1],
-                    getContext().getResources().getDrawable(R.drawable.ic_remove), compoundDrawables[3]);
+
+    private OnTouchListener l;
+    private OnFocusChangeListener f;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (getCompoundDrawables()[2] != null) {
+            boolean tappedX = event.getX() > (getWidth() - getPaddingRight() - clearDrawable
+                    .getIntrinsicWidth());
+            if (tappedX) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    setText("");
+                    if (clearListener != null) {
+                        clearListener.didClearText();
+                    }
+                }
+                return true;
+            }
+        }
+        if (l != null) {
+            return l.onTouch(v, event);
+        }
+        return false;
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(isNotEmpty(getText()));
         } else {
-            setCompoundDrawables(compoundDrawables[0], compoundDrawables[1], null, compoundDrawables[3]);
+            setClearIconVisible(false);
+        }
+        if (f != null) {
+            f.onFocusChange(v, hasFocus);
         }
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        // TODO Auto-generated method stub
-//		DebugUtils.d(TAG, "onTouchEvent is called");
-        if (event.getAction() == MotionEvent.ACTION_UP && dRight != null && mClearable) {
-            rBounds = dRight.getBounds();
-            final int x = (int) event.getX() + this.getLeft();
-            final int y = (int) event.getY();
-//			 System.out.println("x:/y: "+x+"/"+y);
-//			 System.out.println("bounds: "+
-//				 (this.getRight() - rBounds.width())+"/"+
-//				 (this.getRight() - this.getPaddingRight())+"/"+
-//				 this.getPaddingTop()+"/"+
-//				 (this.getHeight() - this.getPaddingBottom()));
-            // check to make sure the touch event was within the bounds of the
-            // drawable
-//			DebugUtils.d(TAG, "dRight is here");
-            if (x >= (this.getRight() - this.getPaddingRight() - rBounds.width())
-                    && x <= (this.getRight() - this.getPaddingRight())
-                    && y >= this.getPaddingTop()
-                    && y <= (this.getHeight() - this.getPaddingBottom())) {
-                // System.out.println("touch");
-                this.setText("");
-				/* use this to prevent the keyboard from coming up */
-                event.setAction(MotionEvent.ACTION_CANCEL);
-//				DebugUtils.d(TAG, "setText is called");
+    public void onTextChanged(CharSequence s, int start, int count, int after) {
+        if (isFocused()) {
+            setClearIconVisible(isNotEmpty(s));
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    private void init() {
+        clearDrawable = getCompoundDrawables()[2];
+        if (clearDrawable == null) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                clearDrawable = getContext().getDrawable(R.drawable.ic_remove_circle);
+            } else {
+                clearDrawable = getResources().getDrawable(R.drawable.ic_remove_circle);
             }
 
         }
-        return super.onTouchEvent(event);
+        clearDrawable.setBounds(0, 0, clearDrawable.getIntrinsicWidth(), clearDrawable.getIntrinsicHeight());
+        setClearIconVisible(false);
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+        addTextChangedListener(this);
     }
 
-    public void autoSetText(CharSequence text) {
-        // TODO Auto-generated method stub
-        super.setText(text);
-        setSelection(getText().length());
+    protected void setClearIconVisible(boolean visible) {
+        boolean wasVisible = (getCompoundDrawables()[2] != null);
+        if (visible != wasVisible) {
+            Drawable x = visible ? clearDrawable : null;
+            setCompoundDrawables(getCompoundDrawables()[0],
+                    getCompoundDrawables()[1], x, getCompoundDrawables()[3]);
+        }
     }
 
+    private boolean isNotEmpty(CharSequence str) {
+        return !TextUtils.isEmpty(str);
+    }
 }
-
