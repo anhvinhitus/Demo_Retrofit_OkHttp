2ff9c8583e08ce88a68c9edba1cf592cf5b718a1
Hiếu Vũ Mạnh <AnhHieu@Hieus-Mac-mini.local>
2016-07-18 11:33:54 +0700

    update gcm

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/notification/MyGcmListenerService.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/notification/MyGcmListenerService.java
@@ -25,27 +25,28 @@ import com.google.gson.Gson;
 
 import timber.log.Timber;
 import vn.com.vng.zalopay.AndroidApplication;
+import vn.com.vng.zalopay.R;
 import vn.com.vng.zalopay.data.cache.UserConfig;
 import vn.com.vng.zalopay.data.ws.model.NotificationData;
 import vn.com.vng.zalopay.domain.model.User;
 import vn.com.vng.zalopay.internal.di.components.UserComponent;
+import vn.com.vng.zalopay.navigation.Navigator;
 
 public class MyGcmListenerService extends GcmListenerService {
 
     private Gson mGson = new Gson();
 
     private UserConfig userConfig = AndroidApplication.instance().getAppComponent().userConfig();
+    private Navigator navigator = AndroidApplication.instance().getAppComponent().navigator();
 
     @Override
     public void onMessageReceived(String from, Bundle data) {
 
+        String message = data.getString("msg");
+        String badgeNumber = data.getString("badgenumber");
+        String embeddata = data.getString("embeddata");
 
-      /*  String message = intent.getStringExtra("msg");
-        String badgeNumber = intent.getStringExtra("badgenumber");
-        String embeddata = intent.getStringExtra("embeddata");*/
-
-        String message = data.getString("message");
-        Timber.d("onMessageReceived: from %s message %s", from, message);
+        Timber.d("onMessageReceived: from %s message %s embeddata %s badgeNumber %s ", from, message, embeddata, badgeNumber);
 
         if (from.startsWith("/topics/")) {
             // message received from some topic.
@@ -53,10 +54,12 @@ public class MyGcmListenerService extends GcmListenerService {
             // normal downstream message.
         }
 
-        NotificationData event = parseMessage(message);
+       /* NotificationData event = parseMessage(message);
         if (event != null) {
             sendNotification(event);
-        }
+        }*/
+
+        sendNotification(message);
     }
 
     private NotificationData parseMessage(String message) {
@@ -88,6 +91,16 @@ public class MyGcmListenerService extends GcmListenerService {
             if (userComponent != null) {
                 NotificationHelper notificationHelper = userComponent.notificationHelper();
                 notificationHelper.processNotification(notify);
+            }
+        }
+    }
+
+    private void sendNotification(String message) {
+        if (!TextUtils.isEmpty(message)) {
+            UserComponent userComponent = AndroidApplication.instance().getUserComponent();
+            if (userComponent != null) {
+                NotificationHelper notificationHelper = userComponent.notificationHelper();
+                notificationHelper.create(getApplicationContext(), 1, navigator.intentHomeActivity(getApplicationContext(), false), R.mipmap.ic_launcher, getString(R.string.app_name), message);
             }
         }
     }
