e04812765fc6882a072ca43321c223a7f938f2a7
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-07-19 16:48:27 +0700

    Refactor PaymentAppConfig

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/navigation/Navigator.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/navigation/Navigator.java
@@ -14,7 +14,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import timber.log.Timber;
-import vn.com.vng.zalopay.ReactAppConfig;
+import vn.com.vng.zalopay.PaymentAppConfig;
 import vn.com.vng.zalopay.Constants;
 import vn.com.vng.zalopay.R;
 import vn.com.vng.zalopay.account.ui.activities.EditProfileActivity;
@@ -297,7 +297,7 @@ public class Navigator implements INavigator {
 
     @Override
     public Intent intentPaymentApp(Context context, int appId, Map<String, String> launchOptions) {
-        AppResource appResource = ReactAppConfig.getAppResource(appId);
+        AppResource appResource = PaymentAppConfig.getAppResource(appId);
         if (appResource == null) {
             return null;
         }
