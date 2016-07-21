a5b5321cd780563fa2fca36208127dc45235c6cd
Long le van <levanlong318@gmail.com>
2016-07-21 17:12:44 +0700

    update param

--- ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/api/response/redpacket/SentPackageResponse.java
+++ ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/api/response/redpacket/SentPackageResponse.java
@@ -6,14 +6,14 @@ import vn.com.vng.zalopay.data.api.response.BaseResponse;
  * Created by longlv on 15/07/2016.
  */
 public class SentPackageResponse extends BaseResponse {
-    public String revZaloPayID;
-    public String revZaloID;
-    public String revFullName;
-    public String revAvatarURL;
-    public long openTime;
+    public String revzaloPayid;
+    public String revzaloid;
+    public String revfullname;
+    public String revavatarurl;
+    public long opentime;
     public long amount;
-    public String sendMessage;
-    public boolean isLuckiest;
+    public String sendmessage;
+    public boolean isluckiest;
 
 
 }
