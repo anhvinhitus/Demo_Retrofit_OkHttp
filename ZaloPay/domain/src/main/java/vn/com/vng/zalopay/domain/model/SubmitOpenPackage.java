7096faadf8951b17eb837c31731929636e00ca5d
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-07-22 10:47:11 +0700

    Cleanup code

--- ZaloPay/domain/src/main/java/vn/com/vng/zalopay/domain/model/SubmitOpenPackage.java
+++ ZaloPay/domain/src/main/java/vn/com/vng/zalopay/domain/model/SubmitOpenPackage.java
@@ -1,45 +1,24 @@
 package vn.com.vng.zalopay.domain.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import org.parceler.Parcel;
 
 /**
  * Created by longlv on 13/07/2016.
  * Relate with RedPackageResponse which is data of request "sendbundle"
  */
-public class SubmitOpenPackage extends AbstractData {
+@Parcel
+public class SubmitOpenPackage {
     public long bundleID;
     public long packageID;
     public long zpTransID;
+
+    public SubmitOpenPackage() {
+
+    }
 
     public SubmitOpenPackage(long bundleID, long packageID, long zpTransID) {
         this.bundleID = bundleID;
         this.packageID = packageID;
         this.zpTransID = zpTransID;
     }
-
-    public SubmitOpenPackage(Parcel in) {
-        bundleID = in.readLong();
-        packageID = in.readLong();
-        zpTransID = in.readLong();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeLong(bundleID);
-        dest.writeLong(packageID);
-        dest.writeLong(zpTransID);
-    }
-
-    public final Parcelable.Creator<SubmitOpenPackage> CREATOR = new Parcelable.Creator<SubmitOpenPackage>() {
-        @Override
-        public SubmitOpenPackage createFromParcel(Parcel source) {
-            return new SubmitOpenPackage(source);
-        }
-
-        @Override
-        public SubmitOpenPackage[] newArray(int size) {
-            return new SubmitOpenPackage[size];
-        }
-    };
 }
--- ZaloPay/domain/src/main/java/vn/com/vng/zalopay/domain/model/redpacket/PackageStatus.java
+++ ZaloPay/domain/src/main/java/vn/com/vng/zalopay/domain/model/redpacket/PackageStatus.java
@@ -1,19 +1,23 @@
 package vn.com.vng.zalopay.domain.model.redpacket;
 
-import android.os.Parcel;
-
-import vn.com.vng.zalopay.domain.model.AbstractData;
+import org.parceler.Parcel;
 
 /**
  * Created by longlv on 18/07/2016.
+ *
  */
-public class PackageStatus extends AbstractData {
+@Parcel
+public class PackageStatus {
     public boolean isProcessing;
     public String zpTransID;
     public long reqdate;
     public long amount;
     public long balance;
     public String data;
+
+    public PackageStatus() {
+
+    }
 
     public PackageStatus(boolean isProcessing, String zpTransID, long reqdate, long amount, long balance, String data) {
         this.isProcessing = isProcessing;
@@ -24,39 +28,4 @@ public class PackageStatus extends AbstractData {
         this.data = data;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeByte(this.isProcessing ? (byte) 1 : (byte) 0);
-        dest.writeString(this.zpTransID);
-        dest.writeLong(this.reqdate);
-        dest.writeLong(this.amount);
-        dest.writeLong(this.balance);
-        dest.writeString(this.data);
-    }
-
-    protected PackageStatus(Parcel in) {
-        this.isProcessing = in.readByte() != 0;
-        this.zpTransID = in.readString();
-        this.reqdate = in.readLong();
-        this.amount = in.readLong();
-        this.balance = in.readLong();
-        this.data = in.readString();
-    }
-
-    public static final Creator<PackageStatus> CREATOR = new Creator<PackageStatus>() {
-        @Override
-        public PackageStatus createFromParcel(Parcel source) {
-            return new PackageStatus(source);
-        }
-
-        @Override
-        public PackageStatus[] newArray(int size) {
-            return new PackageStatus[size];
-        }
-    };
 }
