b45176bb4416c5e709b06311fa93a03c321f8a53
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-07-28 08:38:30 +0700

    Temporary disable Red Packet for production release

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/paymentapps/PaymentAppConfig.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/paymentapps/PaymentAppConfig.java
@@ -30,7 +30,7 @@ public class PaymentAppConfig {
     static {
         APP_RESOURCE_MAP = new HashMap<>();
         APP_RESOURCE_MAP.put(Constants.INTERNAL, new AppResource(Constants.INTERNAL, AndroidApplication.instance().getString(R.string.transfer_money), String.valueOf(R.drawable.ic_chuyentien)));
-        APP_RESOURCE_MAP.put(Constants.RED_PACKET, new AppResource(Constants.RED_PACKET, AndroidApplication.instance().getString(R.string.red_envelope), String.valueOf(R.drawable.ic_lixi)));
+//        APP_RESOURCE_MAP.put(Constants.RED_PACKET, new AppResource(Constants.RED_PACKET, AndroidApplication.instance().getString(R.string.red_envelope), String.valueOf(R.drawable.ic_lixi)));
         APP_RESOURCE_MAP.put(Constants.RECHARGE_MONEY_PHONE, new AppResource(Constants.RECHARGE_MONEY_PHONE, AndroidApplication.instance().getString(R.string.recharge_money_phone), String.valueOf(R.drawable.ic_naptiendt)));
 //        APP_RESOURCE_MAP.put(Constants.BUY_PHONE_CARD, new AppResource(Constants.BUY_PHONE_CARD, AndroidApplication.instance().getString(R.string.buy_phone_card), String.valueOf(R.drawable.ic_muathedt)));
         APP_RESOURCE_MAP.put(Constants.ZING_XU, new AppResource(Constants.ZING_XU, AndroidApplication.instance().getString(R.string.zing_xu), String.valueOf(R.drawable.ic_zingxu)));
@@ -39,7 +39,7 @@ public class PaymentAppConfig {
 
         APP_RESOURCE_LIST = new ArrayList<>();
         APP_RESOURCE_LIST.add(APP_RESOURCE_MAP.get(Constants.INTERNAL));
-        APP_RESOURCE_LIST.add(APP_RESOURCE_MAP.get(Constants.RED_PACKET));
+//        APP_RESOURCE_LIST.add(APP_RESOURCE_MAP.get(Constants.RED_PACKET));
         APP_RESOURCE_LIST.add(APP_RESOURCE_MAP.get(Constants.RECHARGE_MONEY_PHONE));
 //        APP_RESOURCE_LIST.add(APP_RESOURCE_MAP.get(Constants.BUY_PHONE_CARD));
         APP_RESOURCE_LIST.add(APP_RESOURCE_MAP.get(Constants.ZING_XU));
