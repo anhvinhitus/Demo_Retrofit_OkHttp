ce1b63873dd0768391856e7c63b953ff23511388
Long le van <levanlong318@gmail.com>
2016-07-30 16:31:36 +0700

    update min&max

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/Constants.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/Constants.java
@@ -11,8 +11,8 @@ public class Constants {
     public static final long DEFAULT_CONNECTION_TIMEOUT_MINUTES = 2L;
     public static final int MIN_DEPOSIT_MONEY = 20000;
     public static final int MAX_DEPOSIT_MONEY = 10000000;
-    public static final int MIN_TRANSFER_MONEY = 1000;
-    public static final int MAX_TRANSFER_MONEY = 20000000;
+    public static final int MIN_TRANSFER_MONEY = 10000;
+    public static final int MAX_TRANSFER_MONEY = 10000000;
 
     public static final String PREF_USER_SESSION = "pref_user_session";
     public static final String PREF_USER_EXPIREIN = "pref_user_expirein";
