184afc02a3d54c3869352871e2cb1eb76d2796e3
Long le van <levanlong318@gmail.com>
2016-08-02 13:45:07 +0700

    add file

---
+++ ZaloPay/domain/src/main/java/vn/com/vng/zalopay/domain/model/redpacket/AppConfigEntity.java
@@ -0,0 +1,26 @@
+package vn.com.vng.zalopay.domain.model.redpacket;
+
+/**
+ * Created by longlv on 01/08/2016.
+ * Mapping RedPacketAppInfoResponse
+ */
+public class AppConfigEntity {
+
+    public long minAmounTeach;
+    public long maxTotalAmountPerBundle;
+    public int maxPackageQuantity;
+    public int maxCountHist;
+    public int maxMessageLength;
+    public long bundleExpiredTime;
+
+    public AppConfigEntity(long bundleExpiredTime, int maxCountHist,
+                           int maxMessageLength, int maxPackageQuantity,
+                           long maxTotalAmountPerBundle, long minAmounTeach) {
+        this.bundleExpiredTime = bundleExpiredTime;
+        this.maxCountHist = maxCountHist;
+        this.maxMessageLength = maxMessageLength;
+        this.maxPackageQuantity = maxPackageQuantity;
+        this.maxTotalAmountPerBundle = maxTotalAmountPerBundle;
+        this.minAmounTeach = minAmounTeach;
+    }
+}
