65482725ce5197fbe3148971898f41f5b7bbb7f5
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-08-02 14:49:38 +0700

    Precompile sound transcoder to make it faster when build project

--- ZaloPay/soundtranscoder/src/main/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(call all-subdir-makefiles)
-include $(CLEAR_VARS)
-LOCAL_MODULE    := transcoder
-LOCAL_SRC_FILES := crity.cpp filt.cpp fskdemodulation++.cpp SoundDecoder.cpp
-LOCAL_LDLIBS := -llog
-include $(BUILD_SHARED_LIBRARY)
--- ZaloPay/soundtranscoder/src/main/jni/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_OPTIM := release
-APP_STL := gnustl_static
-APP_ABI := armeabi armeabi-v7a x86 mips x86-64 arm64-v8a
-LOCAL_CXXFLAGS += -std=gnu++11
