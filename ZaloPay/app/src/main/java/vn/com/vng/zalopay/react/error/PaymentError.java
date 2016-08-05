4dca646f6e0abe7306d2559e1d45e9b3753f4d4e
Long le van <levanlong318@gmail.com>
2016-08-05 14:07:15 +0700

    refactor

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/react/error/PaymentError.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/react/error/PaymentError.java
@@ -33,30 +33,43 @@ public enum  PaymentError {
     public static String getErrorMessage(PaymentError paymentError) {
         Timber.d("getErrorMessage error [%s]", paymentError);
         String errorMessage;
-        if (paymentError == ERR_CODE_SUCCESS) {
-            errorMessage = "Giao dịch thành công.";
-        } else if (paymentError == ERR_CODE_FAIL) {
-            errorMessage = "Giao dịch thất bại.";
-        } else if (paymentError == ERR_CODE_PROCESSING) {
-            errorMessage = "Giao dịch đang được xử lý.";
-        } else if (paymentError == ERR_CODE_SERVICE_MAINTENANCE) {
-            errorMessage = "Đang bảo trì hệ thống.";
-        } else if (paymentError == PaymentError.ERR_CODE_MONEY_NOT_ENOUGH) {
-            errorMessage = "Tài khoản không đủ để thực hiện giao dịch.";
-        } else if (paymentError == ERR_CODE_INPUT) {
-            errorMessage = "Thông tin đầu vào thiếu hoặc không hợp lệ.";
-        } else if (paymentError == ERR_CODE_INTERNET) {
-            errorMessage = "Vui lòng kiểm tra kết nối mạng và thử lại.";
-        } else if (paymentError == ERR_CODE_SYSTEM) {
-            errorMessage = "Lỗi xảy ra trong quá trình thanh toán. Vui lòng thử lại sau.";
-        } else if (paymentError == ERR_CODE_USER_INFO) {
-            errorMessage = "Thông tin người dùng không hợp lệ.";
-        } else if (paymentError == ERR_CODE_USER_CANCEL) {
-            errorMessage = "Người dùng huỷ bỏ giao dịch.";
-        } else if (paymentError == ERR_CODE_TOKEN_INVALID) {
-            errorMessage = "Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại!";
-        } else {
-            errorMessage = String.format("Lỗi xảy ra trong quá trình thanh toán.[%s]", paymentError);
+        switch (paymentError) {
+            case ERR_CODE_SUCCESS:
+                errorMessage = "Giao dịch thành công.";
+                break;
+            case ERR_CODE_FAIL:
+                errorMessage = "Giao dịch thất bại.";
+                break;
+            case ERR_CODE_PROCESSING:
+                errorMessage = "Giao dịch đang được xử lý.";
+                break;
+            case ERR_CODE_SERVICE_MAINTENANCE:
+                errorMessage = "Đang bảo trì hệ thống.";
+                break;
+            case ERR_CODE_MONEY_NOT_ENOUGH:
+                errorMessage = "Tài khoản không đủ để thực hiện giao dịch.";
+                break;
+            case ERR_CODE_INPUT:
+                errorMessage = "Thông tin đầu vào thiếu hoặc không hợp lệ.";
+                break;
+            case ERR_CODE_INTERNET:
+                errorMessage = "Vui lòng kiểm tra kết nối mạng và thử lại.";
+                break;
+            case ERR_CODE_SYSTEM:
+                errorMessage = "Lỗi xảy ra trong quá trình thanh toán. Vui lòng thử lại sau.";
+                break;
+            case ERR_CODE_USER_INFO:
+                errorMessage = "Thông tin người dùng không hợp lệ.";
+                break;
+            case ERR_CODE_USER_CANCEL:
+                errorMessage = "Người dùng huỷ bỏ giao dịch.";
+                break;
+            case ERR_CODE_TOKEN_INVALID:
+                errorMessage = "Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại!";
+                break;
+            default:
+                errorMessage = String.format("Lỗi xảy ra trong quá trình thanh toán.[%s]", paymentError);
+                break;
         }
         return errorMessage;
     }
