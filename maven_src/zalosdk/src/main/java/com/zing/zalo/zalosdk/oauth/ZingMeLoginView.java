5d856b38e2904f1c847f4c73a72548f1a63e5292
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-08-06 11:42:39 +0700

    Move zalosdk from source to maven_src

---
+++ maven_src/zalosdk/src/main/java/com/zing/zalo/zalosdk/oauth/ZingMeLoginView.java
@@ -0,0 +1,14 @@
+package com.zing.zalo.zalosdk.oauth;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class ZingMeLoginView extends ZingMeBaseLoginView{
+		
+	public ZingMeLoginView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+	}
+}
