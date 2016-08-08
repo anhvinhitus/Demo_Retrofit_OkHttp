9267518fbad3a819673e740044cce1341cb6608e
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-08-08 17:04:27 +0700

    Add timing trackers

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/service/ZPTrackerAnswers.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/service/ZPTrackerAnswers.java
@@ -3,6 +3,7 @@ package vn.com.vng.zalopay.service;
 import com.crashlytics.android.answers.Answers;
 import com.crashlytics.android.answers.ContentViewEvent;
 import com.crashlytics.android.answers.CustomEvent;
+import com.google.android.gms.analytics.HitBuilders;
 
 import vn.com.vng.zalopay.analytics.ZPEvents;
 import vn.com.vng.zalopay.analytics.ZPTracker;
@@ -25,5 +26,19 @@ public class ZPTrackerAnswers implements ZPTracker {
     @Override
     public void trackScreen(String screenName) {
         Answers.getInstance().logContentView(new ContentViewEvent().putContentName(screenName).putContentType("Launch"));
+    }
+
+    /**
+     * Log timing value for given event.
+     * Events are defined in https://docs.google.com/spreadsheets/d/1kdqC78-qMsRGY_n4hMyzUsbHlS3Gl4yWwgr62qLO8Co/edit#gid=0
+     *
+     * @param eventId If of timing event
+     * @param value   time recorded in milliseconds
+     */
+    @Override
+    public void trackTiming(int eventId, long value) {
+        CustomEvent event = new CustomEvent(ZPEvents.categoryFromEventId(eventId))
+                .putCustomAttribute("value", value);
+        Answers.getInstance().logCustom(event);
     }
 }
--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/service/ZPTrackerGA.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/service/ZPTrackerGA.java
@@ -44,4 +44,19 @@ public class ZPTrackerGA implements ZPTracker {
         mTracker.setScreenName(screenWithFormat);
         mTracker.send(new HitBuilders.ScreenViewBuilder().build());
     }
+
+    /**
+     * Log timing value for given event.
+     * Events are defined in https://docs.google.com/spreadsheets/d/1kdqC78-qMsRGY_n4hMyzUsbHlS3Gl4yWwgr62qLO8Co/edit#gid=0
+     *
+     * @param eventId If of timing event
+     * @param value   time recorded in milliseconds
+     */
+    @Override
+    public void trackTiming(int eventId, long value) {
+        mTracker.send(new HitBuilders.TimingBuilder()
+            .setCategory(ZPEvents.categoryFromEventId(eventId))
+            .setValue(value)
+            .build());
+    }
 }
