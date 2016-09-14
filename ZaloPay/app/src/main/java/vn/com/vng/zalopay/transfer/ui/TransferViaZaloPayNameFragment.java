9fc281cb63c9650ac96c6a842c283e84a113f367
Hiếu Vũ Mạnh <AnhHieu@Hieus-Mac-mini.local>
2016-09-14 15:36:01 +0700

    Update UI

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/transfer/ui/TransferViaZaloPayNameFragment.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/transfer/ui/TransferViaZaloPayNameFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
+import android.text.TextUtils;
 import android.view.View;
 
 import org.parceler.Parcels;
@@ -125,6 +126,7 @@ public class TransferViaZaloPayNameFragment extends BaseFragment implements ITra
 
     @Override
     public void showError(String message) {
+        textInputAccountName.setErrorEnabled(!TextUtils.isEmpty(message));
         textInputAccountName.setError(message);
     }
 
