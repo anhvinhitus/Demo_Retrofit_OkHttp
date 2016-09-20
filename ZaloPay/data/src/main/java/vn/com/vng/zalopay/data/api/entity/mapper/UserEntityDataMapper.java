fcdbbe66a35a331586ceccdc0c16f908c6da4d13
Long le van <levanlong318@gmail.com>
2016-09-20 16:44:02 +0700

    remove PermissionEntity

--- ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/api/entity/mapper/UserEntityDataMapper.java
+++ ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/api/entity/mapper/UserEntityDataMapper.java
@@ -1,16 +1,9 @@
 package vn.com.vng.zalopay.data.api.entity.mapper;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import vn.com.vng.zalopay.data.api.entity.PermissionEntity;
 import vn.com.vng.zalopay.data.api.response.LoginResponse;
-import vn.com.vng.zalopay.data.util.Lists;
-import vn.com.vng.zalopay.domain.model.Permission;
 import vn.com.vng.zalopay.domain.model.User;
 
 @Singleton
@@ -35,32 +28,4 @@ public class UserEntityDataMapper {
 
         return user;
     }
-//
-//    public Permission transform(PermissionEntity entity) {
-//        Permission permission = null;
-//        if (entity != null) {
-//            permission = new Permission();
-//            permission.allow = entity.allow;
-//            permission.pmcid = entity.pmcid;
-//            permission.profilelevel = entity.profilelevel;
-//            permission.requireotp = entity.requireotp;
-//            permission.transtype = entity.transtype;
-//            permission.requirepin = entity.requirepin;
-//        }
-//        return permission;
-//    }
-//
-//    public List<Permission> transform(List<PermissionEntity> entities) {
-//        if (Lists.isEmptyOrNull(entities)) {
-//            return Collections.emptyList();
-//        }
-//        List<Permission> permissions = new ArrayList<>();
-//        for (PermissionEntity entity : entities) {
-//            Permission permission = transform(entity);
-//            if (permission != null) {
-//                permissions.add(permission);
-//            }
-//        }
-//        return permissions;
-//    }
 }
