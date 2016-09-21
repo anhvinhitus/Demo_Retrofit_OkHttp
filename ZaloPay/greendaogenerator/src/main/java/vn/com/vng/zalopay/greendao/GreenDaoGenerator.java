5606bcc3117fb9c8ed0295a7395d5e9681d798b2
Hiếu Vũ Mạnh <AnhHieu@Hieus-Mac-mini.local>
2016-09-21 19:47:54 +0700

    Update version database

--- ZaloPay/greendaogenerator/src/main/java/vn/com/vng/zalopay/greendao/GreenDaoGenerator.java
+++ ZaloPay/greendaogenerator/src/main/java/vn/com/vng/zalopay/greendao/GreenDaoGenerator.java
@@ -8,7 +8,7 @@ import de.greenrobot.daogenerator.Schema;
 import de.greenrobot.daogenerator.ToMany;
 
 public class GreenDaoGenerator {
-    private static final int APP_DB_VERSION = 42;
+    private static final int APP_DB_VERSION = 43;
 
     public static void main(String[] args) throws Exception {
         Schema appSchema = new Schema(APP_DB_VERSION, "vn.com.vng.zalopay.data.cache.model");
