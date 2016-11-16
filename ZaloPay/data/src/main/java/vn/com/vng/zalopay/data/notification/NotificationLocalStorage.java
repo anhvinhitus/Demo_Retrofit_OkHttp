11c258a72a5bc508c41713ae98328092f8fd77e1
longlv <levanlong318@gmail.com>
2016-11-16 18:23:50 +0700

    update notification config

--- ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/notification/NotificationLocalStorage.java
+++ ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/notification/NotificationLocalStorage.java
@@ -198,6 +198,8 @@ public class NotificationLocalStorage extends SqlBaseScopeImpl implements Notifi
                         .limit(limit)
                         .offset(pageIndex * limit)
                         .orderDesc(NotificationGDDao.Properties.Timestamp)
+                        .where(NotificationGDDao.Properties.Message.isNotNull())
+                        .where(NotificationGDDao.Properties.Transid.gt(0))
                         .list());
     }
 
