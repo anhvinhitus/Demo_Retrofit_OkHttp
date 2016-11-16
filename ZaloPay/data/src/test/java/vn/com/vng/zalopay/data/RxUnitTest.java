298c2e5aba9225af5d6384660925eadae49629e3
hieuvm <hieuvm@vng.com.vn>
2016-11-16 16:45:57 +0700

    Unit test recursion

--- ZaloPay/data/src/test/java/vn/com/vng/zalopay/data/RxUnitTest.java
+++ ZaloPay/data/src/test/java/vn/com/vng/zalopay/data/RxUnitTest.java
@@ -1,23 +1,21 @@
 package vn.com.vng.zalopay.data;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
 /**
  * Created by huuhoa on 7/5/16.
  */
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 16)
+
 public class RxUnitTest {
     @Test
     public void testJust() {
@@ -103,6 +101,7 @@ public class RxUnitTest {
                     public void onError(Throwable e) {
                         System.out.println("Errr: " + e);
                         latch.countDown();
+                        latch.countDown();
                     }
 
                     @Override
@@ -114,4 +113,87 @@ public class RxUnitTest {
 
         latch.await();
     }
+
+
+    @Test
+    public void testRecurseRx() {
+        Observable<Long> observableJust = Observable.just(1L)
+                .filter(aLong -> aLong > 0)
+                .flatMap(aLong -> Observable.just(3L));
+        observableJust.subscribe(new Subscriber<Long>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted Just");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                System.out.println("along just " + aLong);
+            }
+        });
+
+        Observable<Long> observable = requestApi(0);
+        //  final CountDownLatch latch = new CountDownLatch(1);
+
+        Subscriber<Long> testSubscriber = new Subscriber<Long>() {
+            @Override
+            public void onError(Throwable e) {
+                System.out.println("error: " + e);
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                System.out.println("Recurse time " + aLong);
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted: Recurse");
+            }
+        };
+        observable.subscribe(testSubscriber);
+        //  testSubscriber.assertNoErrors();
+        //Assert.assertTrue(false);
+       /* try {
+            latch.await();
+        } catch (Exception ex) {
+            //empty
+        }*/
+    }
+
+    private Observable<Long> requestApi(long page) {
+        return Observable.just(page)
+                .filter(aLong -> {
+                    System.out.println("along +: " + aLong);
+                    return aLong < 30;
+                })
+                .flatMap(aLong -> requestApi(page + 1))
+                ;
+    }
+
+    @Test
+    public void testRecurseRx2() {
+        int maxPageNum = 20;
+        Subscription subscription = Observable.create(subscriber -> getIntegers(1, 5, maxPageNum, subscriber))
+                .subscribe(System.out::println);
+    }
+
+    private void getIntegers(final int pageStart, int pageSize, int maxPageNumber, Subscriber<? super Integer> subscriber) {
+        Subscription subscription = Observable.range(pageStart, pageSize)
+                .doOnCompleted(() -> {
+                    int newPageStart = pageStart + pageSize;
+                    if (newPageStart >= maxPageNumber) {
+                        subscriber.onCompleted();
+                    } else {
+                        getIntegers(newPageStart, pageSize, maxPageNumber, subscriber);
+                    }
+                })
+                .subscribe(subscriber::onNext);
+    }
+
 }
