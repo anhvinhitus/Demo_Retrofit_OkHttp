92007de4689837369fbfb96e6f5bcd4c7875a0a6
longlv <levanlong318@gmail.com>
2016-11-22 15:42:12 +0700

    remove code unused

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/internal/di/modules/UserApiModule.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/internal/di/modules/UserApiModule.java
@@ -5,12 +5,12 @@ import javax.inject.Named;
 import dagger.Module;
 import dagger.Provides;
 import retrofit2.Retrofit;
-import vn.com.vng.zalopay.data.api.ZaloPayIAPService;
 import vn.com.vng.zalopay.data.api.ZaloPayService;
 import vn.com.vng.zalopay.internal.di.scope.UserScope;
 
 /**
  * Created by AnhHieu on 4/26/16.
+ * *
  */
 
 @Module
@@ -20,13 +20,6 @@ public class UserApiModule {
     @UserScope
     ZaloPayService provideZaloPayService(@Named("retrofitApi") Retrofit retrofit) {
         return retrofit.create(ZaloPayService.class);
-    }
-
-
-    @Provides
-    @UserScope
-    ZaloPayIAPService provideZaloPayIAPService(@Named("retrofitApi") Retrofit retrofit) {
-        return retrofit.create(ZaloPayIAPService.class);
     }
 
 }
