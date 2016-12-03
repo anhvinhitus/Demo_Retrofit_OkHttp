98f4d9075dccd84d7d12a1abb862f1c387b147b3
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-12-04 00:45:30 +0700

    Test new splash screen

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/activity/SplashScreenActivity.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/activity/SplashScreenActivity.java
@@ -1,5 +1,8 @@
 package vn.com.vng.zalopay.ui.activity;
 
+import android.os.Bundle;
+
+import vn.com.vng.zalopay.R;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
 import vn.com.vng.zalopay.ui.fragment.SplashScreenFragment;
 import vn.com.zalopay.analytics.ZPAnalytics;
@@ -20,6 +23,13 @@ public class SplashScreenActivity extends BaseActivity {
     protected void onStart() {
         super.onStart();
         ZPAnalytics.trackEvent(ZPEvents.APP_LAUNCH);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar_FullScreen);
+
+        super.onCreate(savedInstanceState);
     }
 
     @Override
