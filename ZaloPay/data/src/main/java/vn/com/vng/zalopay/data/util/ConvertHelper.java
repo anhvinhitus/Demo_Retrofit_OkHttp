b1e37bd9a370588308b47d52cd026f60de6ffbd4
Nguyen Huu Hoa <huuhoa@gmail.com>
2016-12-06 18:27:15 +0700

    Use explicit version of unboxValue for compile time validation

--- ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/util/ConvertHelper.java
+++ ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/util/ConvertHelper.java
@@ -2,17 +2,33 @@ package vn.com.vng.zalopay.data.util;
 
 /**
  * Created by huuhoa on 12/6/16.
+ * Data converter helper
  */
 
 public class ConvertHelper {
+//
+//    @SuppressWarnings (value="unchecked")
+//    public static <T, U> U unboxValue(T value, U defaultValue) {
+//        if (value == null) {
+//            return defaultValue;
+//        } else {
+//            return (U)value;
+//        }
+//    }
 
-    @SuppressWarnings (value="unchecked")
-    public static <T, U> U unboxValue(T value, U defaultValue) {
+    public static long unboxValue(Long value, long defaultValue) {
         if (value == null) {
             return defaultValue;
         } else {
-            return (U)value;
+            return value;
         }
     }
 
+    public static int unboxValue(Integer value, int defaultValue) {
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return value;
+        }
+    }
 }
