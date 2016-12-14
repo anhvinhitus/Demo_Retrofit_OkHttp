b8dde4f83e3088974aabadb1c26a927bd18a8558
hieuvm <hieuvm@vng.com.vn>
2016-12-14 11:51:16 +0700

    Update font loader

---
+++ maven_src/reactnativemodules/src/main/java/com/zalopay/modules/FontLoaderModule.java
@@ -0,0 +1,36 @@
+package com.zalopay.modules;
+
+import android.graphics.Typeface;
+import android.net.Uri;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.views.text.ReactFontManager;
+
+import java.io.File;
+
+public class FontLoaderModule extends ReactContextBaseJavaModule {
+    public FontLoaderModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ZaloPayFontLoader";
+    }
+
+    @ReactMethod
+    public void loadAsync(final String fontFamilyName, final String localUri, final Promise promise) {
+        try {
+            // TODO(nikki): make sure path is in experience's scope
+            Typeface typeface = Typeface.createFromFile(new File(Uri.parse(localUri).getPath()));
+            ReactFontManager.getInstance().setTypeface(fontFamilyName, Typeface.NORMAL, typeface);
+            promise.resolve(Arguments.createMap());
+        } catch (Exception e) {
+            promise.reject(e);
+        }
+    }
+}
