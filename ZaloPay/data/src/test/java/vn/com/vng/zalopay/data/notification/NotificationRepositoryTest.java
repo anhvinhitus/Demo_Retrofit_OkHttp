2e340ea0ed13228abee5881deddca3c9a0da85b3
nhakha <khattn2@vng.com.vn>
2016-12-26 18:07:31 +0700

    Update unit test NotificationRepository

--- ZaloPay/data/src/test/java/vn/com/vng/zalopay/data/notification/NotificationRepositoryTest.java
+++ ZaloPay/data/src/test/java/vn/com/vng/zalopay/data/notification/NotificationRepositoryTest.java
@@ -2,65 +2,28 @@ package vn.com.vng.zalopay.data.notification;
 
 import android.database.sqlite.SQLiteDatabase;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import org.greenrobot.eventbus.EventBus;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.annotation.Config;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.http.Field;
 import retrofit2.http.Query;
 import rx.Observable;
 import vn.com.vng.zalopay.data.ApplicationTestCase;
-import vn.com.vng.zalopay.data.BuildConfig;
-import vn.com.vng.zalopay.data.CustomObserver;
-import vn.com.vng.zalopay.data.CustomRobolectricRunner;
 import vn.com.vng.zalopay.data.DefaultObserver;
-import vn.com.vng.zalopay.data.api.entity.UserRPEntity;
-import vn.com.vng.zalopay.data.api.entity.mapper.RedPacketDataMapper;
 import vn.com.vng.zalopay.data.api.response.BaseResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.BundleOrderResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.GetReceivePackageResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.PackageInBundleResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.PackageStatusResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.ReceivePackageResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.RedPacketAppInfoResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.SentBundleListResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.SentBundleResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.SentPackageInBundleResponse;
-import vn.com.vng.zalopay.data.api.response.redpacket.SubmitOpenPackageResponse;
 import vn.com.vng.zalopay.data.cache.model.DaoMaster;
 import vn.com.vng.zalopay.data.cache.model.DaoSession;
-import vn.com.vng.zalopay.data.cache.model.GetReceivePacket;
-import vn.com.vng.zalopay.data.cache.model.PackageInBundleGD;
-import vn.com.vng.zalopay.data.cache.model.ReceivePackageGD;
-import vn.com.vng.zalopay.data.cache.model.SentBundleGD;
-import vn.com.vng.zalopay.data.redpacket.RedPacketLocalStorage;
-import vn.com.vng.zalopay.data.redpacket.RedPacketRepository;
-import vn.com.vng.zalopay.data.redpacket.RedPacketStore;
 import vn.com.vng.zalopay.data.rxbus.RxBus;
 import vn.com.vng.zalopay.data.ws.model.NotificationData;
 import vn.com.vng.zalopay.data.ws.model.NotificationEmbedData;
 import vn.com.vng.zalopay.domain.model.User;
-import vn.com.vng.zalopay.domain.model.redpacket.AppConfigEntity;
-import vn.com.vng.zalopay.domain.model.redpacket.BundleOrder;
-import vn.com.vng.zalopay.domain.model.redpacket.GetSentBundle;
-import vn.com.vng.zalopay.domain.model.redpacket.PackageInBundle;
-import vn.com.vng.zalopay.domain.model.redpacket.PackageStatus;
-import vn.com.vng.zalopay.domain.model.redpacket.ReceivePackage;
-import vn.com.vng.zalopay.domain.model.redpacket.RedPacketAppInfo;
-import vn.com.vng.zalopay.domain.model.redpacket.SentBundle;
-import vn.com.vng.zalopay.domain.model.redpacket.SubmitOpenPackage;
-
-import static org.junit.Assert.assertEquals;
 
 public class NotificationRepositoryTest extends ApplicationTestCase {
     NotificationStore.Repository mRepository;
@@ -108,8 +71,8 @@ public class NotificationRepositoryTest extends ApplicationTestCase {
             entity.userid = "abc";
             entity.notificationstate = 1;
             entity.notificationtype = 1;
-            entity.mtaid = 1;
-            entity.mtuid = 0;
+            entity.mtaid = 0;
+            entity.mtuid = j;
 
             entities.add(entity);
         }
@@ -120,7 +83,127 @@ public class NotificationRepositoryTest extends ApplicationTestCase {
     }
 
     @Test
-    public void testIt() {
-        assertEquals(true, true);
+    public void get() {
+        initData();
+        mRepository.putNotify(entities).subscribe();
+
+        List<NotificationData> result = new ArrayList<>();
+        int pageIndex = 2;
+        int limit = 5;
+
+        mRepository.getNotification(pageIndex, limit).subscribe(new DefaultObserver<>(result));
+        assertEquals(entities.subList(
+                TRANSACTION_SIZE - (pageIndex + 1) * limit, TRANSACTION_SIZE - (pageIndex + 1) * limit + limit), result);
+    }
+
+    @Test
+    public void getAllTransactions() {
+        initData();
+        mRepository.putNotify(entities).subscribe();
+
+        List<NotificationData> result = new ArrayList<>();
+        int pageIndex = 0;
+        int limit = TRANSACTION_SIZE;
+
+        mRepository.getNotification(pageIndex, limit).subscribe(new DefaultObserver<>(result));
+        assertEquals(entities, result);
+    }
+
+    @Test
+    public void getZeroTransaction() {
+        initData();
+        mRepository.putNotify(entities);
+
+        List<NotificationData> result = new ArrayList<>();
+        int pageIndex = 0;
+        int limit = 0;
+
+        mRepository.getNotification(pageIndex, limit).subscribe(new DefaultObserver<>(result));
+        Assert.assertEquals("getNotification 0 transaction", 0, result.size());
+    }
+
+    @Test
+    public void getWithEmptyDB() {
+        List<NotificationData> result = new ArrayList<>();
+        int pageIndex = 0;
+        int limit = TRANSACTION_SIZE;
+
+        mRepository.getNotification(pageIndex, limit).subscribe(new DefaultObserver<>(result));
+        Assert.assertEquals("getNotification with empty DB", 0, result.size());
+    }
+
+    @Test
+    public void getOversizedList() {
+        initData();
+        mRepository.putNotify(entities);
+
+        List<NotificationData> result = new ArrayList<>();
+        int pageIndex = 5;
+        int limit = 5;
+
+        mRepository.getNotification(pageIndex, limit).subscribe(new DefaultObserver<>(result));
+        Assert.assertEquals("getNotification oversized list", 0, result.size());
+    }
+
+    @Test
+    public void getTransactionWhenLimitIsANegativeNumber() {
+        initData();
+        mRepository.putNotify(entities);
+
+        List<NotificationData> result = new ArrayList<>();
+        int pageIndex = 5;
+        int limit = -1;
+
+        mRepository.getNotification(pageIndex, limit).subscribe(new DefaultObserver<>(result));
+        Assert.assertEquals("getNotification with limit is a negative number", 0, result.size());
+    }
+
+    @Test
+    public void getTransactionWhenPageIndexIsANegativeNumber() {
+        initData();
+        mRepository.putNotify(entities);
+
+        List<NotificationData> result = new ArrayList<>();
+        int pageIndex = -1;
+        int limit = 4;
+
+        mRepository.getNotification(pageIndex, limit).subscribe(new DefaultObserver<>(result));
+        Assert.assertEquals("getNotification transaction with pageIndex is a negative number", 0, result.size());
+    }
+
+    private void assertEquals(List<NotificationData> list1, List<NotificationData> list2) {
+        if(list2.size() == 0) {
+            Assert.fail("object is null");
+            return;
+        }
+
+        for (int i = 0; i < list2.size(); i++) {
+            compare2Elements(list1.get(i), list2.get(list2.size() - i - 1));
+        }
+    }
+
+    private void compare2Elements(NotificationData b1, NotificationData b2) {
+        if (b1 == null && b2 != null) {
+            Assert.fail("Compare null and non-null object");
+            return;
+        }
+
+        if (b1 != null && b2 == null) {
+            Assert.fail("Compare null and non-null object");
+            return;
+        }
+
+        Assert.assertEquals("appid", b1.appid, b2.appid);
+        Assert.assertEquals("area", b1.area, b2.area);
+        Assert.assertEquals("destuserid", b1.destuserid, b2.destuserid);
+//        Assert.assertEquals("embeddata", b1.embeddata, b2.embeddata);
+        Assert.assertEquals("message", b1.message, b2.message);
+        Assert.assertEquals("timestamp", b1.timestamp, b2.timestamp);
+        Assert.assertEquals("notificationId", b1.notificationId, b2.notificationId);
+        Assert.assertEquals("transid", b1.transid, b2.transid);
+        Assert.assertEquals("userid", b1.userid, b2.userid);
+        Assert.assertEquals("notificationstate", b1.notificationstate, b2.notificationstate);
+        Assert.assertEquals("notificationtype", b1.notificationtype, b2.notificationtype);
+        Assert.assertEquals("appid", b1.transtype, b2.transtype);
     }
 }
