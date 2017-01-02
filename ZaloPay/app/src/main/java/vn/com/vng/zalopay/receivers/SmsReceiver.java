058fe2bb05c45c89c53ab405cd8483bbf5233799
Nguyen Huu Hoa <huuhoa@gmail.com>
2017-01-02 11:31:19 +0700

    Fix crash on SmsReceiver (https://fabric.io/zalo-pay/android/apps/vn.com.vng.zalopay/issues/5868b67e0aeb16625ba50f2d)

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/receivers/SmsReceiver.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/receivers/SmsReceiver.java
@@ -19,37 +19,41 @@ import vn.com.vng.zalopay.event.ReceiveSmsEvent;
 public class SmsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        //---get the SMS message passed in---
-        Bundle bundle = intent.getExtras();
-        SmsMessage[] msgs = null;
-        String str = "";
-        if (bundle != null) {
+        try {
+            //---get the SMS message passed in---
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] msgs = null;
+            String str = "";
+            if (bundle != null) {
 
-            ReceiveSmsEvent event = new ReceiveSmsEvent();
-            //---retrieve the SMS message received---
-            Object[] pdus = (Object[]) bundle.get("pdus");
-            if (pdus != null) {
-                msgs = new SmsMessage[pdus.length];
-            }
-            if (msgs != null) {
-                for (int i = 0; i < msgs.length; i++) {
-                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                    if (msgs[i] == null) {
-                        continue;
-                    }
-                    str += "SMS from " + msgs[i].getOriginatingAddress();
-                    str += " :";
-                    str += msgs[i].getMessageBody();
-                    event.addMessage(msgs[i].getOriginatingAddress(), msgs[i].getMessageBody());
-                    str += "\n";
+                ReceiveSmsEvent event = new ReceiveSmsEvent();
+                //---retrieve the SMS message received---
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if (pdus != null) {
+                    msgs = new SmsMessage[pdus.length];
                 }
-            }
-            EventBus eventBus = AndroidApplication.instance().getAppComponent().eventBus();
-            eventBus.removeStickyEvent(ReceiveSmsEvent.class);
-            eventBus.postSticky(event);
+                if (msgs != null) {
+                    for (int i = 0; i < msgs.length; i++) {
+                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                        if (msgs[i] == null) {
+                            continue;
+                        }
+                        str += "SMS from " + msgs[i].getOriginatingAddress();
+                        str += " :";
+                        str += msgs[i].getMessageBody();
+                        event.addMessage(msgs[i].getOriginatingAddress(), msgs[i].getMessageBody());
+                        str += "\n";
+                    }
+                }
+                EventBus eventBus = AndroidApplication.instance().getAppComponent().eventBus();
+                eventBus.removeStickyEvent(ReceiveSmsEvent.class);
+                eventBus.postSticky(event);
 
-            //---display the new SMS message---
-            Timber.d(str);
+                //---display the new SMS message---
+                Timber.d(str);
+            }
+        } catch (Throwable t) {
+            Timber.w(t, "Got exception while processing received sms");
         }
     }
 }
