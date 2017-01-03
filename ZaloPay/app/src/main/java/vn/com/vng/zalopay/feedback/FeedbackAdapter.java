e4ff69255b6f46f3276d43f5deeadda5fe94bae6
nhakha <khattn2@vng.com.vn>
2017-01-03 18:38:05 +0700

    Update Feedback UI: add recyclerview

---
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/feedback/FeedbackAdapter.java
@@ -0,0 +1,153 @@
+package vn.com.vng.zalopay.feedback;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.zalopay.ui.widget.recyclerview.AbsRecyclerAdapter;
+import com.zalopay.ui.widget.recyclerview.OnItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import timber.log.Timber;
+import vn.com.vng.zalopay.R;
+
+/**
+ * Created by cpu11759-local on 03/01/2017.
+ */
+
+public class FeedbackAdapter extends AbsRecyclerAdapter<Integer, RecyclerView.ViewHolder> {
+    private static final int FOOTER_VIEW = 1;
+
+    private FeedbackAdapter.OnClickAddListener addListener;
+    private FeedbackAdapter.OnClickDeleteListener deleteListener;
+
+    public FeedbackAdapter(Context context, FeedbackAdapter.OnClickAddListener addListener,
+                           FeedbackAdapter.OnClickDeleteListener deleteListener) {
+        super(context);
+        this.addListener = addListener;
+        this.deleteListener = deleteListener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == FOOTER_VIEW) {
+            return new FeedbackAdapter.FooterViewHolder(mInflater
+                    .inflate(R.layout.layout_add_screenshot, parent, false), addListener);
+        }
+
+        return new FeedbackAdapter.ViewHolder(mInflater
+                .inflate(R.layout.row_card_image_feedback, parent, false), deleteListener);
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+        addListener = null;
+        deleteListener = null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof FeedbackAdapter.ViewHolder) {
+            Integer screen = getItem(position);
+            if (screen != null) {
+                ((FeedbackAdapter.ViewHolder) holder).bindView(screen);
+            }
+        }
+//        else if (holder instanceof FooterViewHolder) {
+//            FooterViewHolder footerViewHolder = (FooterViewHolder) holder;
+//        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mItems == null) {
+            return 0;
+        }
+
+        if (mItems.size() == 0) {
+            return 1;
+        }
+
+        if(mItems.size() == getContext().getResources().getInteger(R.integer.max_length_add_screenshot)) {
+            return mItems.size();
+        }
+
+        return mItems.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == mItems.size()) {
+            return FOOTER_VIEW;
+        }
+
+        return super.getItemViewType(position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_image)
+        ImageView imgScreen;
+
+        @BindView(R.id.iv_delete)
+        ImageView mDelete;
+
+        public ViewHolder(View itemView, final OnClickDeleteListener listener) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            mDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onClickDelete(getPosition());
+                }
+            });
+        }
+
+        public void bindView(Integer screen) {
+            if (imgScreen == null) {
+                return;
+            }
+            if (screen == 0) {
+                imgScreen.setImageDrawable(null);
+            } else {
+                imgScreen.setImageResource(screen);
+            }
+        }
+    }
+
+    public class FooterViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.container)
+        LinearLayout container;
+
+        public FooterViewHolder(View itemView, final OnClickAddListener listener) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onClickAdd();
+                }
+            });
+        }
+    }
+
+    public interface OnClickAddListener {
+        void onClickAdd();
+    }
+
+    public interface OnClickDeleteListener {
+        void onClickDelete(int position);
+    }
+}
--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/feedback/FeedbackFragment.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/feedback/FeedbackFragment.java
@@ -2,24 +2,31 @@ package vn.com.vng.zalopay.feedback;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import vn.com.vng.zalopay.R;
+import vn.com.vng.zalopay.account.ui.fragment.AbsPickerImageFragment;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
 
-public class FeedbackFragment extends BaseFragment {
+public class FeedbackFragment extends BaseFragment implements
+        FeedbackAdapter.OnClickAddListener, FeedbackAdapter.OnClickDeleteListener {
 
-    public FeedbackFragment() {
-        // Required empty public constructor
-    }
+    private FeedbackAdapter mAdapter;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment WarningRootedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    @BindView(R.id.txtTitleImage)
+    TextView tvTitleImage;
+    @BindView(R.id.listView)
+    RecyclerView mRecyclerView;
+
     public static FeedbackFragment newInstance() {
         return new FeedbackFragment();
     }
@@ -35,12 +42,88 @@ public class FeedbackFragment extends BaseFragment {
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        mAdapter = new FeedbackAdapter(getContext(), this, this);
     }
 
     @Override
-    public boolean onBackPressed() {
-        return true;
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        List<Integer> tmp = new ArrayList<>();
+//        tmp.add(R.drawable.ic_agribank);
+//        tmp.add(R.drawable.ic_bidv);
+//        tmp.add(R.drawable.ic_chungchi);
+//        tmp.add(R.drawable.ic_eximbank);
+        setData(tmp);
+    }
+
+    @Override
+    public void onDestroyView() {
+//        mPresenter.detachView();
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+//        mPresenter.destroy();
+        mAdapter = null;
+        super.onDestroy();
+    }
+
+    public void setData(List<Integer> images) {
+        mAdapter.setData(images);
+        setImageCount();
+    }
+
+    public void updateData(Integer image) {
+        mAdapter.insert(image);
+        setImageCount();
+    }
+
+    public void removeData(int position) {
+        mAdapter.remove(position);
+        setImageCount();
+    }
+
+    @Override
+    public void onClickAdd() {
+        showBottomSheetDialog();
+    }
+
+    @Override
+    public void onClickDelete(int position) {
+        removeData(position);
+    }
+
+    private void showBottomSheetDialog() {
+        AbsPickerImageFragment.CoverBottomSheetDialogFragment dialog = AbsPickerImageFragment.CoverBottomSheetDialogFragment.newInstance();
+        dialog.setOnClickListener(new AbsPickerImageFragment.CoverBottomSheetDialogFragment.OnClickListener() {
+            @Override
+            public void onClickCapture() {
+//                startCaptureImage(requestCode, getImageNameFromReqCode(requestCode));
+            }
+
+            @Override
+            public void onClickGallery() {
+//                startPickImage(requestCode);
+            }
+        });
+        dialog.show(getChildFragmentManager(), "bottomsheet");
+    }
+
+    private void setImageCount() {
+        int size = mAdapter.getItemCount() - 1;
+        if(mAdapter.getItemViewType(size) == 0) {
+            size = size + 1;
+        }
+        tvTitleImage.setText(String.format(getResources().getString(R.string.txt_attach_screen),
+                String.valueOf(size)));
     }
 }
