c5aec0c0d83f3995ee2b229f1cad1e968d397191
hieuvm <hieuvm@vng.com.vn>
2017-01-04 11:54:50 +0700

    add timeout get zalo

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/utils/ZaloHelper.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/utils/ZaloHelper.java
@@ -1,12 +1,15 @@
 package vn.com.vng.zalopay.utils;
 
 import android.content.Context;
+import android.os.CountDownTimer;
 
 import com.zing.zalo.zalosdk.oauth.ZaloOpenAPICallback;
 import com.zing.zalo.zalosdk.oauth.ZaloSDK;
 
 import org.json.JSONObject;
 
+import rx.Observable;
+import rx.Subscriber;
 import timber.log.Timber;
 import vn.com.vng.zalopay.data.cache.UserConfig;
 
@@ -17,12 +20,16 @@ import vn.com.vng.zalopay.data.cache.UserConfig;
 
 public class ZaloHelper {
     private static ZaloOpenAPICallback mCallback = null;
-    public static void getZaloProfileInfo(Context context, final UserConfig userConfig) {
+
+
+    public static void getZaloProfileInfo(final Context context, final UserConfig userConfig) {
         Timber.d("request Zalo Profile");
         if (mCallback != null) {
             Timber.w("Get Zalo Profile is already in progress");
             return;
         }
+
+        startTimeout(5000);
 
         mCallback = new ZaloOpenAPICallback() {
             @Override
@@ -34,10 +41,37 @@ public class ZaloHelper {
                     Timber.w(ex, " Exception :");
                 }
 
+                stopTimeout();
                 mCallback = null;
             }
         };
 
         ZaloSDK.Instance.getProfile(context, mCallback);
+    }
+
+    private static CountDownTimer mCountDownTimer;
+
+    private static void startTimeout(long timeMillis) {
+        stopTimeout();
+        mCountDownTimer = new CountDownTimer(timeMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                Timber.d("Get zalo profile timeout");
+                mCallback = null;
+            }
+        };
+        mCountDownTimer.start();
+    }
+
+    private static void stopTimeout() {
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+            mCountDownTimer = null;
+        }
+
     }
 }
