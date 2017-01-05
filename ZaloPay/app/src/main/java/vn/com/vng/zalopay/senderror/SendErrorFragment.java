8136c1ef903ff9833e09a589ba6507291051d91b
khattn <khattn2@vng.com.vn>
2017-01-05 13:31:13 +0700

    Update SendError: collect device, app, user information

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/senderror/SendErrorFragment.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/senderror/SendErrorFragment.java
@@ -5,11 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SwitchCompat;
-import android.text.Editable;
 import android.text.Html;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.zalopay.ui.widget.edittext.ZPEditText;
@@ -18,39 +17,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
+import butterknife.OnFocusChange;
+import butterknife.OnTextChanged;
+import timber.log.Timber;
 import vn.com.vng.zalopay.R;
 import vn.com.vng.zalopay.account.ui.fragment.AbsPickerImageFragment;
+import vn.com.vng.zalopay.data.UserCollector;
+import vn.com.vng.zalopay.data.cache.UserConfig;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
 import vn.com.vng.zalopay.ui.widget.validate.EmailValidate;
+import vn.zalopay.feedback.FeedbackCollector;
+import vn.zalopay.feedback.collectors.AppCollector;
+import vn.zalopay.feedback.collectors.DeviceCollector;
+import vn.zalopay.feedback.collectors.NetworkCollector;
 
 public class SendErrorFragment extends BaseFragment implements
-        SendErrorAdapter.OnClickAddListener, SendErrorAdapter.OnClickDeleteListener, View.OnClickListener {
+        SendErrorAdapter.OnClickAddListener, SendErrorAdapter.OnClickDeleteListener, SwitchCompat.OnCheckedChangeListener {
 
     private SendErrorAdapter mAdapter;
+    private FeedbackCollector mCollector;
+    private UserCollector mUserCollector;
+    private AppCollector mAppCollector;
+    private DeviceCollector mDeviceCollector;
 
     @BindView(R.id.tvTransactionType)
-    TextView tvTransactionType;
+    TextView mTvTransactionType;
     @BindView(R.id.edtTransactionId)
-    ZPEditText edtTransactionId;
+    ZPEditText mEdtTransactionId;
     @BindView(R.id.edtEmail)
-    ZPEditText edtEmail;
+    ZPEditText mEdtEmail;
     @BindView(R.id.edtDescribe)
-    ZPEditText edtDescribe;
+    ZPEditText mEdtDescribe;
 
     @BindView(R.id.swSendUserInfor)
-    SwitchCompat swSendUserInfor;
+    SwitchCompat mSwSendUserInfor;
     @BindView(R.id.swSendDeviceInfor)
-    SwitchCompat swSendDeviceInfor;
+    SwitchCompat mSwSendDeviceInfor;
     @BindView(R.id.swSendAppInfor)
-    SwitchCompat swSendAppInfor;
+    SwitchCompat mSwSendAppInfor;
 
     @BindView(R.id.txtTitleImage)
-    TextView tvTitleImage;
+    TextView mTvTitleImage;
     @BindView(R.id.listView)
     RecyclerView mRecyclerView;
 
     @BindView(R.id.btnSend)
-    Button btnSend;
+    Button mBtnSend;
 
     public static SendErrorFragment newInstance() {
         return new SendErrorFragment();
@@ -71,72 +84,32 @@ public class SendErrorFragment extends BaseFragment implements
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         mAdapter = new SendErrorAdapter(getContext(), this, this);
+
+        mCollector = new FeedbackCollector();
+        mUserCollector = new UserCollector(getAppComponent().userConfig());
+        mAppCollector = new AppCollector(this.getActivity());
+        mDeviceCollector = new DeviceCollector();
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
-        setTransactionType("Rút tiền");
-        setTransactionId("160810000000064");
-        List<Integer> tmp = new ArrayList<>();
-        tmp.add(R.drawable.ic_agribank);
-        tmp.add(R.drawable.ic_bidv);
-        tmp.add(R.drawable.ic_chungchi);
-//        tmp.add(R.drawable.ic_eximbank);
-        setScreenshot(tmp);
+        mSwSendUserInfor.setOnCheckedChangeListener(this);
+        mSwSendDeviceInfor.setOnCheckedChangeListener(this);
+        mSwSendAppInfor.setOnCheckedChangeListener(this);
 
-        edtEmail.addValidator(new EmailValidate(getString(R.string.email_invalid)));
+        mEdtEmail.addValidator(new EmailValidate(getString(R.string.email_invalid)));
 
-        edtDescribe.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {
+        collectInformation();
+        setDefaultEmail();
 
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                edtDescribe.setFloatingLabelText(Html.fromHtml(
-                        String.format(getResources().getString(R.string.describe_error_count),
-                        String.valueOf(edtDescribe.getText().length()))));
-            }
-        });
-    }
-
-    @Override
-    public void onDestroyView() {
-//        mPresenter.detachView();
-        super.onDestroyView();
-    }
-
-    @Override
-    public void onDestroy() {
-//        mPresenter.destroy();
-        mAdapter = null;
-        super.onDestroy();
-    }
-
-    @Override
-    public void onClick(View view) {
-        int itemId = view.getId();
-        if (itemId == R.id.swSendUserInfor) {
-
-        } else if (itemId == R.id.swSendDeviceInfor) {
-
-        } else if (itemId == R.id.swSendAppInfor) {
-
-        } else if (itemId == R.id.btnSend) {
-
-        }
+        testData();
     }
 
     public void setScreenshot(List<Integer> images) {
@@ -155,11 +128,84 @@ public class SendErrorFragment extends BaseFragment implements
     }
 
     public void setTransactionType(String type) {
-        tvTransactionType.setText(type);
+        if(type != null) {
+            mTvTransactionType.setText(type);
+        }
     }
 
     public void setTransactionId(String id) {
-        edtTransactionId.setText(id);
+        if(id != null) {
+            mEdtTransactionId.setText(id);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+//        mPresenter.detachView();
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+//        mPresenter.destroy();
+        mAdapter = null;
+        super.onDestroy();
+    }
+
+    @OnTextChanged(value = R.id.edtDescribe, callback = OnTextChanged.Callback.TEXT_CHANGED)
+    public void onTextChangedDescribe() {
+        mEdtDescribe.setFloatingLabelText(Html.fromHtml(
+                String.format(getResources().getString(R.string.describe_error_count),
+                        String.valueOf(mEdtDescribe.getText().length()))));
+    }
+
+    @OnTextChanged(value = R.id.edtDescribe, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    public void afterTextChangedDescribe() {
+        mBtnSend.setEnabled(mEdtEmail.isValid() && mEdtDescribe.isValid());
+    }
+
+    @OnTextChanged(value = R.id.edtEmail, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+    public void afterTextChangedEmail() {
+        mBtnSend.setEnabled(mEdtEmail.isValid() && mEdtDescribe.isValid());
+    }
+
+    @OnFocusChange({R.id.edtEmail, R.id.edtDescribe})
+    public void onFocusChange(View v, boolean hasView) {
+        Timber.d("onFocusChange %s", hasView);
+        mBtnSend.setEnabled(mEdtEmail.isValid() && mEdtDescribe.isValid());
+    }
+
+    @OnClick(R.id.btnSend)
+    public void onClickSend() {
+        if (!mEdtEmail.validate() || !mEdtDescribe.validate()) {
+            return;
+        }
+
+        mCollector.startCollectors();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        int itemId = compoundButton.getId();
+        if (itemId == R.id.swSendUserInfor) {
+            if(b == false) {
+                mCollector.removeCollector(mUserCollector);
+            } else if(b == true) {
+                mCollector.installCollector(mUserCollector);
+            }
+        } else if (itemId == R.id.swSendDeviceInfor) {
+            if(b == false) {
+                mCollector.removeCollector(mDeviceCollector);
+            } else if(b == true) {
+                mCollector.installCollector(mDeviceCollector);
+            }
+        } else if (itemId == R.id.swSendAppInfor) {
+            if(b == false) {
+                mCollector.removeCollector(mAppCollector);
+            } else if(b == true) {
+                mCollector.installCollector(mAppCollector);
+            }
+        }
     }
 
     @Override
@@ -193,7 +239,34 @@ public class SendErrorFragment extends BaseFragment implements
         if(mAdapter.getItemViewType(size) == 0) {
             size = size + 1;
         }
-        tvTitleImage.setText(String.format(getResources().getString(R.string.txt_attach_screen),
+
+        mTvTitleImage.setText(String.format(getResources().getString(R.string.txt_attach_screen),
                 String.valueOf(size)));
+    }
+
+    private void setDefaultEmail() {
+        UserConfig currentUser = getAppComponent().userConfig();
+        String email = currentUser.getCurrentUser().email;
+        if(email != null) {
+            mEdtEmail.setText(email);
+        }
+    }
+
+    private void collectInformation() {
+        mCollector.installCollector(mUserCollector);
+        mCollector.installCollector(mAppCollector);
+        mCollector.installCollector(mDeviceCollector);
+        mCollector.installCollector(new NetworkCollector(this.getActivity()));
+    }
+
+    private void testData() {
+        setTransactionType("Rút tiền");
+        setTransactionId("160810000000064");
+        List<Integer> tmp = new ArrayList<>();
+        tmp.add(R.drawable.ic_agribank);
+//        tmp.add(R.drawable.ic_bidv);
+//        tmp.add(R.drawable.ic_chungchi);
+//        tmp.add(R.drawable.ic_eximbank);
+        setScreenshot(tmp);
     }
 }
