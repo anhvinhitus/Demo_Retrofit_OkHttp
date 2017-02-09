2a2be8e462f0b93bbd5c217ec9d8dcac6f4e338c
Long le van <levanlong318@gmail.com>
2017-02-09 16:14:36 +0700

    change User-Agent from url to header

--- ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/qrcode/QRCodeStore.java
+++ ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/qrcode/QRCodeStore.java
@@ -3,7 +3,7 @@ package vn.com.vng.zalopay.data.qrcode;
 import com.google.gson.JsonObject;
 
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Header;
 import retrofit2.http.Url;
 import rx.Observable;
 
@@ -16,7 +16,7 @@ public interface QRCodeStore {
 
     interface RequestService {
         @GET
-        Observable<JsonObject> getPaymentInfo(@Url String url, @Query("ua") String ua);
+        Observable<JsonObject> getPaymentInfo(@Url String url, @Header("User-Agent") String ua);
     }
 
     interface Repository {
