e35088592eef16c38fb456eb74c4adff8e2f57db
hieuvm <hieuvm@vng.com.vn>
2017-02-20 22:07:53 +0700

    Clean code

--- ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/Constants.java
+++ ZaloPay/data/src/main/java/vn/com/vng/zalopay/data/Constants.java
@@ -8,9 +8,6 @@ public class Constants {
 
     public static final String MANIF_BALANCE = "manif_balance";
 
-    public static final String MANIF_PLATFORM_INFO_CHECKSUM = "manif_pfinfochecksum";
-    public static final String MANIF_RESOURCE_VERSION = "manif_resource_version";
-
     public static final String MANIFEST_RESOURCE_INTERNAL_VERSION = "manifest_resource_internal_version";
     public static final String MANIFEST_RESOURCE_EXTERNAL_VERSION = "manifest_resource_internal_version";
 
