e347a72c0a67f5043c1cbf0c9b51534f49f21875
Nguyen Huu Hoa <huuhoa@gmail.com>
2017-02-21 19:22:14 +0700

    [WIP] Try WebAppActivity with different theme

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppActivity.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppActivity.java
@@ -1,9 +1,9 @@
 package vn.com.vng.zalopay.webapp;
 
-import vn.com.vng.zalopay.ui.activity.BaseToolBarActivity;
+import vn.com.vng.zalopay.ui.activity.BaseActivity;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
 
-public class WebAppActivity extends BaseToolBarActivity {
+public class WebAppActivity extends BaseActivity {
     @Override
     public BaseFragment getFragmentToHost() {
         return WebAppFragment.newInstance(getIntent().getExtras());
