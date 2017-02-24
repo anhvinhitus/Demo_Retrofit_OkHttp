d88df4a819850a3b8b952d08e352644f362206ac
khattn <khattn2@vng.com.vn>
2017-02-24 10:10:51 +0700

    Change action bar to tool bar WebAppActivity

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppActivity.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppActivity.java
@@ -1,11 +1,30 @@
 package vn.com.vng.zalopay.webapp;
 
-import vn.com.vng.zalopay.ui.activity.BaseActivity;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import vn.com.vng.zalopay.R;
+import vn.com.vng.zalopay.ui.activity.BaseToolBarActivity;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
 
-public class WebAppActivity extends BaseActivity {
+public class WebAppActivity extends BaseToolBarActivity {
     @Override
     public BaseFragment getFragmentToHost() {
         return WebAppFragment.newInstance(getIntent().getExtras());
+    }
+
+    @Override
+    protected int getResLayoutId() {
+        return R.layout.activity_common_actionbar_white;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Drawable backArrow = getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp);
+        backArrow.setColorFilter(getResources().getColor(R.color.colorWebAppPrimaryText), PorterDuff.Mode.SRC_ATOP);
+        getToolbar().setNavigationIcon(backArrow);
     }
 }
