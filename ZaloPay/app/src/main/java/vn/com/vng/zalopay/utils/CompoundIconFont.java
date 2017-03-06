b782d7365907b1e48aa203f3440ed5e0400c4763
khattn <khattn2@vng.com.vn>
2017-03-06 16:23:19 +0700

    [Compound Drawable] Update CompoundIconFont

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/utils/CompoundIconFont.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/utils/CompoundIconFont.java
@@ -18,21 +18,22 @@ import com.zalopay.ui.widget.R;
 
 /**
  * Created by khattn on 3/2/17.
+ *
  */
 
 public class CompoundIconFont extends TextView {
 
-    IconFontDrawable mIconFontDrawable;
-    final int[] mLeftIcon = {R.styleable.CompoundIconFont_iconLeftName,
+    IconFontDrawable mLeftIcon, mRightIcon, mTopIcon, mBottomIcon;
+    final int[] mLeftValue = {R.styleable.CompoundIconFont_iconLeftName,
             R.styleable.CompoundIconFont_iconLeftSize,
             R.styleable.CompoundIconFont_iconLeftColor};
-    final int[] mRightIcon = {R.styleable.CompoundIconFont_iconRightName,
+    final int[] mRightValue = {R.styleable.CompoundIconFont_iconRightName,
             R.styleable.CompoundIconFont_iconRightSize,
             R.styleable.CompoundIconFont_iconRightColor};
-    final int[] mTopIcon = {R.styleable.CompoundIconFont_iconTopName,
+    final int[] mTopValue = {R.styleable.CompoundIconFont_iconTopName,
             R.styleable.CompoundIconFont_iconTopSize,
             R.styleable.CompoundIconFont_iconTopColor};
-    final int[] mBottomIcon = {R.styleable.CompoundIconFont_iconBottomName,
+    final int[] mBottomValue = {R.styleable.CompoundIconFont_iconBottomName,
             R.styleable.CompoundIconFont_iconBottomSize,
             R.styleable.CompoundIconFont_iconBottomColor};
 
@@ -50,28 +51,22 @@ public class CompoundIconFont extends TextView {
             return;
         }
 
-        mIconFontDrawable = new IconFontDrawable(context);
+        mLeftIcon = new IconFontDrawable(context);
+        mRightIcon = new IconFontDrawable(context);
+        mTopIcon = new IconFontDrawable(context);
+        mBottomIcon = new IconFontDrawable(context);
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CompoundIconFont);
-        Drawable[] drawables = getCompoundDrawables();
 
         if (typedArray == null) {
             return;
         }
 
-        if (initIcon(typedArray, mLeftIcon[0], mLeftIcon[1], mLeftIcon[2])) {
-
-            setCompoundDrawablesWithIntrinsicBounds(mIconFontDrawable, drawables[1], drawables[2], drawables[3]);
-        } else if (initIcon(typedArray, mRightIcon[0], mRightIcon[1], mRightIcon[2])) {
-
-            setCompoundDrawablesWithIntrinsicBounds(drawables[0], drawables[1], mIconFontDrawable, drawables[3]);
-        } else if (initIcon(typedArray, mTopIcon[0], mTopIcon[1], mTopIcon[2])) {
-
-            setCompoundDrawablesWithIntrinsicBounds(drawables[0], mIconFontDrawable, drawables[2], drawables[3]);
-        } else if (initIcon(typedArray, mBottomIcon[0], mBottomIcon[1], mBottomIcon[2])) {
-
-            setCompoundDrawablesWithIntrinsicBounds(drawables[0], drawables[1], drawables[2], mIconFontDrawable);
-        }
+        initIcon(mLeftIcon, typedArray, mLeftValue[0], mLeftValue[1], mLeftValue[2]);
+        initIcon(mRightIcon, typedArray, mRightValue[0], mRightValue[1], mRightValue[2]);
+        initIcon(mTopIcon, typedArray, mTopValue[0], mTopValue[1], mTopValue[2]);
+        initIcon(mBottomIcon, typedArray, mBottomValue[0], mBottomValue[1], mBottomValue[2]);
+        setCompoundDrawablesWithIntrinsicBounds(mLeftIcon, mTopIcon, mRightIcon, mBottomIcon);
 
         try {
             typedArray.recycle();
@@ -80,14 +75,11 @@ public class CompoundIconFont extends TextView {
         }
     }
 
-    private boolean initIcon(TypedArray typedArray, int name, int size, int color) {
-        boolean isLocation = false;
-
+    private void initIcon(IconFontDrawable icon, TypedArray typedArray, int name, int size, int color) {
         try {
             String iconName = typedArray.getString(name);
             if (iconName != null) {
-                mIconFontDrawable.setIcon(iconName);
-                isLocation = true;
+                icon.setIcon(iconName);
             }
         } catch (RuntimeException e) {
             Timber.d(e, "set font and icon name throw RuntimeException");
@@ -96,8 +88,7 @@ public class CompoundIconFont extends TextView {
         try {
             int iconSize = typedArray.getDimensionPixelSize(size, -1);
             if (iconSize >= 0) {
-                mIconFontDrawable.setPxSize(iconSize);
-                isLocation = true;
+                icon.setPxSize(iconSize);
             }
         } catch (UnsupportedOperationException e) {
             Timber.d(e, "get icon size throw UnsupportedOperationException");
@@ -108,45 +99,28 @@ public class CompoundIconFont extends TextView {
         try {
             int iconColor = typedArray.getResourceId(color, -1);
             if (iconColor >= 0) {
-                mIconFontDrawable.setResourcesColor(iconColor);
-                isLocation = true;
+                icon.setResourcesColor(iconColor);
             }
         } catch (UnsupportedOperationException e) {
             Timber.d(e, "get icon color throw UnsupportedOperationException");
         } catch (RuntimeException e) {
             Timber.d(e, "get icon color throw RuntimeException");
         }
-
-        return isLocation;
     }
 
-    public void setIconColor(int color) {
-        try {
-            mIconFontDrawable.setResourcesColor(color);
-        } catch (Resources.NotFoundException e) {
-            Timber.w(e, "setIconColor throw NotFoundException");
-        }
+    public IconFontDrawable getLeftIcon() {
+        return mLeftIcon;
     }
 
-    public void setIconColor(String color) {
-        if (TextUtils.isEmpty(color)) {
-            return;
-        }
-        mIconFontDrawable.setColor(Color.parseColor(color));
+    public IconFontDrawable getRightIcon() {
+        return mRightIcon;
     }
 
-    public void setIcon(Integer iconResource) {
-        mIconFontDrawable.setIcon(iconResource);
+    public IconFontDrawable getTopIcon() {
+        return mTopIcon;
     }
 
-    public void setIcon(String iconName) {
-        if (TextUtils.isEmpty(iconName)) {
-            setText("");
-        } else {
-            IconFontInfo iconFontInfo = IconFontHelper.getInstance().getIconFontInfo(iconName);
-            if (iconFontInfo != null) {
-                mIconFontDrawable.setIcon(iconFontInfo.code);
-            }
-        }
+    public IconFontDrawable getBottomIcon() {
+        return mBottomIcon;
     }
 }
