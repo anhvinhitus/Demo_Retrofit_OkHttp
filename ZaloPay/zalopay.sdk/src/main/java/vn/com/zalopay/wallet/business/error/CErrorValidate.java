15c2c5d6176936d0bb5265b9f829626aed38c761
chucvv <chucvv@vng.com.vn>
2017-03-15 09:56:03 +0700

    [SDK] Thêm mã phân biệt giao dịch thất bại do vượt hạn mức

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/error/CErrorValidate.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/error/CErrorValidate.java
@@ -22,54 +22,37 @@ public class CErrorValidate {
         mErrorArray.put(-2, "APPID_INVALID");
         mErrorArray.put(-3, "APP_NOT_AVAILABLE");
         mErrorArray.put(-4, "APP_TIME_INVALID");
-
         mErrorArray.put(-5, "AMOUNT_INVALID");
         mErrorArray.put(-6, "PLATFORM_INVALID");
         mErrorArray.put(-7, "PLATFORM_NOT_AVAILABLE");
         mErrorArray.put(-8, "DSCREEN_TYPE_INVALID");
-
         mErrorArray.put(-70, "APPTRANSID_EXIST");
         mErrorArray.put(-79, "DUPLICATE_ZPTRANSID");
-
         mErrorArray.put(-18, "DEVICEID_NOT_MATCH");
         mErrorArray.put(-19, "APPID_NOT_MATCH");
         mErrorArray.put(-20, "PLATFORM_NOT_MATCH");
         mErrorArray.put(-21, "PMC_FACTORY_NOT_FOUND");
-
         mErrorArray.put(-32, "APP_INACTIVE");
         mErrorArray.put(-33, "APP_MAINTENANCE");
-
         mErrorArray.put(-36, "OVER_LIMIT");
-
         mErrorArray.put(-81, "USER_NOT_MATCH");
-
         mErrorArray.put(-40, "MAX_RETRY_GET_DBG_STATUS");
-
         mErrorArray.put(-48, "WRONG_PASSWORD");
-
         mErrorArray.put(-68, "DUPLICATE_APPTRANSID");
-
         mErrorArray.put(-52, "ITEMS_INVALID");
         mErrorArray.put(-53, "HMAC_INVALID");
         mErrorArray.put(-54, "TIME_INVALID");
         mErrorArray.put(-57, "APP_USER_INVALID");
         mErrorArray.put(-66, "USER_INVALID");
-
         mErrorArray.put(-83, "TRANSID_FORMAT_INVALID");
         mErrorArray.put(-84, "CARD_TOKEN_INVALID");
         mErrorArray.put(-85, "CARD_TOKEN_EXPIRE");
-
         mErrorArray.put(-89, "MAP_APPID_APPTRANSID_FAIL");
-
         mErrorArray.put(-92, "APPTRANSID_INVALID");
-
-        //mErrorArray.put(-95, "CARD_ALREADY_MAP");
-
         mErrorArray.put(-60, "ZPW_ACCOUNT_NAME_INVALID");
         mErrorArray.put(-61, "ZPW_ACCOUNT_SUSPENDED");
         mErrorArray.put(-62, "ZPW_ACCOUNT_NOT_EXIST");
         mErrorArray.put(-63, "ZPW_BALANCE_NOT_ENOUGH");
-
         mErrorArray.put(2, "DUPLICATE");
         mErrorArray.put(5, "PROCESSING");
 
@@ -82,8 +65,6 @@ public class CErrorValidate {
         mErrorAccountArray.put(-124, "USER_IS_LOCKED");
 
     }
-
-    ;
 
     public static boolean showDialogWithStatus() {
         if (GlobalData.getPaymentResult() != null &&
