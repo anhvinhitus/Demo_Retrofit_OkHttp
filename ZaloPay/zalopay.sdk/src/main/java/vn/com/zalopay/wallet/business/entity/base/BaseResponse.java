847c75b6c0fec4190d11034ef73bef046a7a5dd5
chucvv <chucvv@vng.com.vn>
2017-03-17 17:16:23 +0700

    [SDK] clear code unusage

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/base/BaseResponse.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/base/BaseResponse.java
@@ -6,12 +6,10 @@ public class BaseResponse extends BaseEntity<BaseResponse> {
     public int returncode = 0;
     public String returnmessage = null;
     public String accesstoken = null;
-
     public String suggest_message = null;
     public String suggest_actions = null;
 
-    public String getMessage()
-    {
-        return !TextUtils.isEmpty(suggest_message) ? suggest_message: returnmessage;
+    public String getMessage() {
+        return !TextUtils.isEmpty(suggest_message) ? suggest_message : returnmessage;
     }
 }
