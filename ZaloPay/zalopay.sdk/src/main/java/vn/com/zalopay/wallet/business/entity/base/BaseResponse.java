cc05dd610bcb526da44f01034000fc14f5c89e62
chucvv <chucvv@vng.com.vn>
2017-03-17 17:16:23 +0700

    [SDK] clear code unusage

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/base/BaseResponse.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/base/BaseResponse.java
@@ -4,7 +4,6 @@ public class BaseResponse extends BaseEntity<BaseResponse> {
     public int returncode = 0;
     public String returnmessage = null;
     public String accesstoken = null;
-
     public String suggest_message = null;
     public String suggest_actions = null;
 
--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/base/StatusResponse.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/base/StatusResponse.java
@@ -1,16 +1,11 @@
 package vn.com.zalopay.wallet.business.entity.base;
 
-
 import vn.com.zalopay.wallet.business.entity.atm.DAtmScriptOutput;
 
 public class StatusResponse extends BaseResponse {
-
     public boolean isprocessing = false;
-
     public String data = null;
-
     public String zptransid;
-
     public StatusResponse() {
         data = null;
         zptransid = null;
@@ -27,14 +22,6 @@ public class StatusResponse extends BaseResponse {
         this.returnmessage = pScriptOutput.message;
         this.suggest_message = pScriptOutput.message;
         this.isprocessing = !pScriptOutput.shouldStop;
-    }
-
-    public StatusResponse(BaseResponse pBaseResponse) {
-        this.data = null;
-        this.returncode = pBaseResponse.returncode;
-        this.returnmessage = pBaseResponse.returnmessage;
-        this.suggest_message = pBaseResponse.returnmessage;
-        this.isprocessing = false;
     }
 
     public StatusResponse(int pCode, String pMessage) {
