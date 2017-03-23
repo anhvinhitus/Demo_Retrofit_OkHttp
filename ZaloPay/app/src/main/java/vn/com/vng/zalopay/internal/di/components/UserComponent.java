cc3405258dbdb7a0ab1f45394aa1c1252a01fe29
khattn <khattn2@vng.com.vn>
2017-03-23 11:14:44 +0700

    Revert "[Log Location] Add track location class and location in database"
    This reverts commit 7bb2b26a3268df0771ade5b813b0305931f70072.

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/internal/di/components/UserComponent.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/internal/di/components/UserComponent.java
@@ -26,7 +26,6 @@ import vn.com.vng.zalopay.bank.ui.BankSupportLinkCardDialog;
 import vn.com.vng.zalopay.banner.ui.fragment.BannerFragment;
 import vn.com.vng.zalopay.data.balance.BalanceStore;
 import vn.com.vng.zalopay.data.cache.AccountStore;
-import vn.com.vng.zalopay.data.location.LocationStore;
 import vn.com.vng.zalopay.data.paymentconnector.PaymentConnectorService;
 import vn.com.vng.zalopay.data.transaction.TransactionStore;
 import vn.com.vng.zalopay.domain.model.User;
@@ -36,7 +35,6 @@ import vn.com.vng.zalopay.feedback.FeedbackFragment;
 import vn.com.vng.zalopay.bank.ui.BankSupportFragment;
 import vn.com.vng.zalopay.bank.ui.LinkAccountFragment;
 import vn.com.vng.zalopay.internal.di.modules.QRCodeModule;
-import vn.com.vng.zalopay.internal.di.modules.UserLocationModule;
 import vn.com.vng.zalopay.internal.di.modules.UserSocketModule;
 import vn.com.vng.zalopay.protect.ui.ProtectAccountFragment;
 import vn.com.vng.zalopay.internal.di.modules.UserAccountModule;
@@ -99,8 +97,7 @@ import vn.com.vng.zalopay.withdraw.ui.fragment.WithdrawFragment;
                 UserFriendModule.class,
                 UserRedPacketModule.class,
                 QRCodeModule.class,
-                UserSocketModule.class,
-                UserLocationModule.class
+                UserSocketModule.class
 
         }
 )
@@ -111,8 +108,6 @@ public interface UserComponent {
     UserSession userSession();
 
     AccountStore.Repository accountRepository();
-
-    LocationStore.Repository locationRepository();
 
     ZaloPayRepository zaloPayRepository();
 
