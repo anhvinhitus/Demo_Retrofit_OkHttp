9370727c9c75631c3725909b886470cba2549092
chucvv <chucvv@vng.com.vn>
2017-04-08 13:21:49 +0700

    [SDK] Remove test case

--- ZaloPay/zalopay.sdk.apptest/src/test/java/vn/com/vng/zalopay/ZaloPayUnitTest.java
+++ ZaloPay/zalopay.sdk.apptest/src/test/java/vn/com/vng/zalopay/ZaloPayUnitTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertTrue;
 public class ZaloPayUnitTest {
     @Test
     public void testCardNumberWithSpecialCharacter() {
-        assertTrue(CardValidation.validCardName("VO  **"));
+        assertTrue(CardValidation.validCardName("VO "));
     }
 
     @Test
@@ -41,30 +41,6 @@ public class ZaloPayUnitTest {
     @Test
     public void testClearOTP() {
         assertEquals(PaymentUtils.clearOTP("1234-5678"), "12345678");
-    }
-
-    @Test
-    public void testVCBUtils_getVcbType() {
-        assertEquals(VcbUtils.getVcbType("Quý khách vui lòng nhập tên truy cập!"), EVCBType.EMPTY_USERNAME);
-        assertEquals(VcbUtils.getVcbType("Quý khách vui lòng nhập mật khẩu!"), EVCBType.EMPTY_PASSWORD);
-        assertEquals(VcbUtils.getVcbType("Quý khách vui lòng nhập mã xác nhận!"), EVCBType.EMPTY_CAPCHA);
-        assertEquals(VcbUtils.getVcbType("Tên truy cập hoặc mật khẩu không chính xác. Quý khách lưu ý, dịch vụ bị tạm khóa nếu Quý khách nhập sai mật khẩu quá 5 lần."), EVCBType.WRONG_USERNAME_PASSWORD);
-        assertEquals(VcbUtils.getVcbType("Quý khách đã nhập sai mật khẩu quá 5 lần. Dịch vụ đã bị khóa. Để mở lại dịch vụ, Quý khách vui lòng liên hệ Trung tâm dịch vụ khách hàng Vietcombank tại số 1900 54 54 13 hoặc Quầy giao dịch Vietcombank gần nhất để được trợ giúp"), EVCBType.ACCOUNT_LOCKED);
-        assertEquals(VcbUtils.getVcbType("Mã kiểm tra không chính xác! Quý khách vui lòng kiểm tra lại!"), EVCBType.WRONG_CAPTCHA);
-    }
-
-    @Test
-    public void testGetFirstStringWithSize() {
-        assertEquals(StringUtil.getFirstStringWithSize("0412000416723", 4), "0412");
-        assertEquals(StringUtil.getFirstStringWithSize("0412000416723", 6), "041200");
-        assertEquals(StringUtil.getFirstStringWithSize("0412000416723", 0), "");
-    }
-
-    @Test
-    public void testGetLastStringWithSize() {
-        assertEquals(StringUtil.getLastStringWithSize("0412000416723", 4), "6723");
-        assertEquals(StringUtil.getLastStringWithSize("0412000416723", 6), "416723");
-        assertEquals(StringUtil.getLastStringWithSize("0412000416723", 0), "");
     }
 
     @Test
