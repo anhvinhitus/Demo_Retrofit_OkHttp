1ff52d8881083445310f05d1ea2d139c2cc9637e
hieuvm <hieuvm@vng.com.vn>
2017-04-25 18:09:10 +0700

    [App] Refactor code BottomNavigatorView

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/activity/HomeActivity.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/activity/HomeActivity.java
@@ -1,6 +1,7 @@
 package vn.com.vng.zalopay.ui.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
@@ -18,9 +19,11 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.OnPageChange;
+import timber.log.Timber;
 import vn.com.vng.zalopay.R;
 import vn.com.vng.zalopay.react.base.AbstractReactActivity;
 import vn.com.vng.zalopay.react.base.HomePagerAdapter;
+import vn.com.vng.zalopay.ui.widget.HomeBottomNavigationView;
 import vn.com.vng.zalopay.widget.FragmentLifecycle;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
 import vn.com.vng.zalopay.ui.presenter.HomePresenter;
@@ -47,15 +50,11 @@ public class HomeActivity extends AbstractReactActivity implements IHomeView {
     @BindView(R.id.pager)
     ViewPager mViewPager;
 
-    private int currentPosition = 0;
+    private int mCurrentPosition = 0;
 
     @BindView(R.id.navigation)
-    BottomNavigationView mBottomNavigationView;
-
-    // TODO: 4/4/17 - longlv: hardcode for test.
-    private boolean mShowIconNewPromotion = true;
-    private View mIconNewPromotion;
-
+    HomeBottomNavigationView mBottomNavigationView;
+    
     HomePagerAdapter mHomePagerAdapter;
 
     @Override
@@ -83,39 +82,13 @@ public class HomeActivity extends AbstractReactActivity implements IHomeView {
         super.onCreate(savedInstanceState);
         mPresenter.attachView(this);
         mPresenter.initialize();
-
         mHomePagerAdapter = new HomePagerAdapter(getSupportFragmentManager());
         ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) mViewPager.getLayoutParams();
-        params.setMargins(0, getStatusBarHeight(), 0, (int) AndroidUtils.dpToPixels(this, 56));
-
+        params.setMargins(0, AndroidUtils.getStatusBarHeight(this), 0, (int) AndroidUtils.dpToPixels(this, 56));
         mViewPager.setAdapter(mHomePagerAdapter);
         mViewPager.setOffscreenPageLimit(mHomePagerAdapter.getCount() - 1);
-
-        initTabIconFont();
-
-        //Use this function to show titles of all menu elements when bottomNavigationBar has 4 tabs.
-        BottomNavigationViewHelper.disableShiftMode(mBottomNavigationView);
-
-        mBottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.menu_home:
-                    mViewPager.setCurrentItem(HomePagerAdapter.TAB_MAIN_INDEX);
-                    break;
-                case R.id.menu_nearby:
-                    mViewPager.setCurrentItem(HomePagerAdapter.TAB_SHOW_SHOW_INDEX);
-                    break;
-                case R.id.menu_promotion:
-                    mViewPager.setCurrentItem(HomePagerAdapter.TAB_PROMOTION_INDEX);
-                    break;
-                case R.id.menu_profile:
-                    mViewPager.setCurrentItem(HomePagerAdapter.TAB_PERSONAL_INDEX);
-                    break;
-            }
-            setPromotionNewState(item.getItemId() == R.id.menu_promotion);
-            return true;
-        });
-
-        changeBottomNavigationLayout();
+        mBottomNavigationView.setViewPager(mViewPager);
+        selectTabMenu(getIntent());
     }
 
 
@@ -127,26 +100,14 @@ public class HomeActivity extends AbstractReactActivity implements IHomeView {
             ((FragmentLifecycle) fragmentToShow).onStartFragment();
         }
 
-        Fragment fragmentToHide = mHomePagerAdapter.getPage(currentPosition);
+        Fragment fragmentToHide = mHomePagerAdapter.getPage(mCurrentPosition);
         if (fragmentToHide instanceof FragmentLifecycle) {
             ((FragmentLifecycle) fragmentToHide).onStopFragment();
         }
 
-        currentPosition = newPosition;
+        mCurrentPosition = newPosition;
     }
 
-
-    private void changeBottomNavigationLayout() {
-        int paddingBottom = (int) getResources().getDimension(R.dimen.spacing_tiny_s);
-        View tabHome = mBottomNavigationView.findViewById(R.id.menu_home);
-        tabHome.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
-        View tabNearby = mBottomNavigationView.findViewById(R.id.menu_nearby);
-        tabNearby.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
-        View tabPromotion = mBottomNavigationView.findViewById(R.id.menu_promotion);
-        tabPromotion.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
-        View tabProfile = mBottomNavigationView.findViewById(R.id.menu_profile);
-        tabProfile.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
-    }
 
     private void trackEvent(int position) {
         switch (position) {
@@ -200,82 +161,6 @@ public class HomeActivity extends AbstractReactActivity implements IHomeView {
         super.onDestroy();
     }
 
-    private StateListDrawable createStateListDrawable(@StringRes int iconNameActive,
-                                                      @ColorRes int iconColorActive,
-                                                      @StringRes int iconNameNormal,
-                                                      @ColorRes int iconColorNormal) {
-        StateListDrawable stateListDrawable = new StateListDrawable();
-        stateListDrawable.addState(new int[]{android.R.attr.state_checked},
-                new BottomNavigationDrawable(this, iconNameActive, iconColorActive));
-        stateListDrawable.addState(new int[]{},
-                new BottomNavigationDrawable(this, iconNameNormal, iconColorNormal));
-        return stateListDrawable;
-    }
-
-    private void initTabIconFont() {
-        Menu menu = mBottomNavigationView.getMenu();
-
-        menu.getItem(HomePagerAdapter.TAB_MAIN_INDEX).setIcon(
-                createStateListDrawable(R.string.tab_home_active, R.color.colorPrimary,
-                        R.string.tab_home, R.color.txt_item_sub));
-
-        menu.getItem(HomePagerAdapter.TAB_SHOW_SHOW_INDEX).setIcon(
-                createStateListDrawable(R.string.tab_showshow_active, R.color.colorPrimary,
-                        R.string.tab_showshow, R.color.txt_item_sub));
-
-        menu.getItem(HomePagerAdapter.TAB_PROMOTION_INDEX).setIcon(
-                createStateListDrawable(R.string.tab_promotion_active, R.color.colorPrimary,
-                        R.string.tab_promotion, R.color.txt_item_sub));
-
-        menu.getItem(HomePagerAdapter.TAB_PERSONAL_INDEX).setIcon(
-                createStateListDrawable(R.string.tab_personal_active, R.color.colorPrimary,
-                        R.string.tab_personal, R.color.txt_item_sub));
-
-        setPromotionNewState(mViewPager.getCurrentItem() == HomePagerAdapter.TAB_PROMOTION_INDEX);
-    }
-
-    private void setPromotionNewState(boolean isActive) {
-        FrameLayout tabPromotion = (FrameLayout) mBottomNavigationView.findViewById(R.id.menu_promotion);
-        if (isActive) {
-            if (mShowIconNewPromotion) {
-                mShowIconNewPromotion = false;
-                removeIconNew(tabPromotion);
-            }
-        } else {
-            if (mShowIconNewPromotion) {
-                addIconNew(tabPromotion);
-            }
-        }
-    }
-
-    private void addIconNew(FrameLayout frameLayout) {
-        if (mIconNewPromotion != null) {
-            return;
-        }
-        mIconNewPromotion = getLayoutInflater().inflate(R.layout.icon_new, null);
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
-        frameLayout.addView(mIconNewPromotion, -1, layoutParams);
-    }
-
-    private void removeIconNew(FrameLayout frameLayout) {
-        if (frameLayout == null || mIconNewPromotion == null) {
-            return;
-        }
-        frameLayout.removeView(mIconNewPromotion);
-        mIconNewPromotion = null;
-    }
-
-    private int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
-    }
-
     @Override
     public void showLoading() {
         DialogHelper.showLoading(this);
@@ -298,6 +183,20 @@ public class HomeActivity extends AbstractReactActivity implements IHomeView {
 
     @Override
     public void refreshIconFont() {
-        initTabIconFont();
+        if (mBottomNavigationView != null) {
+            mBottomNavigationView.initTabIconFont();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        selectTabMenu(intent);
+    }
+
+    private void selectTabMenu(Intent intent) {
+        int tab_menu = intent.getIntExtra("tab_menu", -1);
+        if (tab_menu >= 0 && tab_menu < mHomePagerAdapter.getCount()) {
+            mBottomNavigationView.setSelected(tab_menu);
+        }
     }
 }
---
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/widget/HomeBottomNavigationView.java
@@ -0,0 +1,170 @@
+package vn.com.vng.zalopay.ui.widget;
+
+import android.content.Context;
+import android.graphics.drawable.StateListDrawable;
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import vn.com.vng.zalopay.R;
+import vn.com.vng.zalopay.react.base.HomePagerAdapter;
+import vn.com.vng.zalopay.utils.AndroidUtils;
+import vn.com.vng.zalopay.utils.BottomNavigationViewHelper;
+
+/**
+ * Created by hieuvm on 4/25/17.
+ * *
+ */
+
+public class HomeBottomNavigationView extends BottomNavigationView implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    public HomeBottomNavigationView(Context context) {
+        this(context, null);
+    }
+
+    public HomeBottomNavigationView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public HomeBottomNavigationView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void addLineTop() {
+        View line = new View(getContext());
+        line.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.separate));
+        addView(line, new LayoutParams(LayoutParams.MATCH_PARENT, AndroidUtils.dp(0.5F)));
+    }
+
+    private void init() {
+        inflateMenu(R.menu.bottom_navigation_items);
+        changeBottomNavigationLayout();
+        initTabIconFont();
+        addLineTop();
+        BottomNavigationViewHelper.disableShiftMode(this);
+    }
+
+
+    private void changeBottomNavigationLayout() {
+        int paddingBottom = (int) getResources().getDimension(R.dimen.spacing_tiny_s);
+        View tabHome = findViewById(R.id.menu_home);
+        tabHome.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
+        View tabNearby = findViewById(R.id.menu_nearby);
+        tabNearby.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
+        View tabPromotion = findViewById(R.id.menu_promotion);
+        tabPromotion.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
+        View tabProfile = findViewById(R.id.menu_profile);
+        tabProfile.findViewById(android.support.design.R.id.icon).setPadding(0, 0, 0, paddingBottom);
+    }
+
+    private StateListDrawable createStateListDrawable(@StringRes int iconNameActive,
+                                                      @ColorRes int iconColorActive,
+                                                      @StringRes int iconNameNormal,
+                                                      @ColorRes int iconColorNormal) {
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(new int[]{android.R.attr.state_checked},
+                new BottomNavigationDrawable(getContext(), iconNameActive, iconColorActive));
+        stateListDrawable.addState(new int[]{},
+                new BottomNavigationDrawable(getContext(), iconNameNormal, iconColorNormal));
+        return stateListDrawable;
+    }
+
+    public void initTabIconFont() {
+        Menu menu = getMenu();
+
+        menu.getItem(HomePagerAdapter.TAB_MAIN_INDEX).setIcon(
+                createStateListDrawable(R.string.tab_home_active, R.color.colorPrimary,
+                        R.string.tab_home, R.color.txt_item_sub));
+
+        menu.getItem(HomePagerAdapter.TAB_SHOW_SHOW_INDEX).setIcon(
+                createStateListDrawable(R.string.tab_showshow_active, R.color.colorPrimary,
+                        R.string.tab_showshow, R.color.txt_item_sub));
+
+        menu.getItem(HomePagerAdapter.TAB_PROMOTION_INDEX).setIcon(
+                createStateListDrawable(R.string.tab_promotion_active, R.color.colorPrimary,
+                        R.string.tab_promotion, R.color.txt_item_sub));
+
+        menu.getItem(HomePagerAdapter.TAB_PERSONAL_INDEX).setIcon(
+                createStateListDrawable(R.string.tab_personal_active, R.color.colorPrimary,
+                        R.string.tab_personal, R.color.txt_item_sub));
+    }
+
+    public void setSelected(int position) {
+        if (mPager == null) {
+            throw new RuntimeException("Viewpager not initialized");
+        }
+        mPager.setCurrentItem(position, false);
+    }
+
+    private ViewPager mPager;
+
+    public void setViewPager(@NonNull ViewPager pager) {
+        mPager = pager;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (mPager == null) {
+            return false;
+        }
+
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                setSelected(HomePagerAdapter.TAB_MAIN_INDEX);
+                return true;
+            case R.id.menu_nearby:
+                setSelected(HomePagerAdapter.TAB_SHOW_SHOW_INDEX);
+                return true;
+            case R.id.menu_promotion:
+                setSelected(HomePagerAdapter.TAB_PROMOTION_INDEX);
+                return true;
+            case R.id.menu_profile:
+                setSelected(HomePagerAdapter.TAB_PERSONAL_INDEX);
+                return true;
+        }
+
+        return false;
+    }
+
+
+     /*   private void setPromotionNewState(boolean isActive) {
+        FrameLayout tabPromotion = (FrameLayout) mBottomNavigationView.findViewById(R.id.menu_promotion);
+        if (isActive) {
+            if (mShowIconNewPromotion) {
+                mShowIconNewPromotion = false;
+                removeIconNew(tabPromotion);
+            }
+        } else {
+            if (mShowIconNewPromotion) {
+                addIconNew(tabPromotion);
+            }
+        }
+    }*/
+
+  /*  private void addIconNew(FrameLayout frameLayout) {
+        if (mIconNewPromotion != null) {
+            return;
+        }
+        mIconNewPromotion = getLayoutInflater().inflate(R.layout.icon_new, null);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
+        frameLayout.addView(mIconNewPromotion, -1, layoutParams);
+    }
+
+    private void removeIconNew(FrameLayout frameLayout) {
+        if (frameLayout == null || mIconNewPromotion == null) {
+            return;
+        }
+        frameLayout.removeView(mIconNewPromotion);
+        mIconNewPromotion = null;
+    }*/
+}
