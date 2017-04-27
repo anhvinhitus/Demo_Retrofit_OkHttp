23720b8a89a4f170c16d8c82a7bc3fa8e9af5f00
hieuvm <hieuvm@vng.com.vn>
2017-04-27 11:21:39 +0700

    [App] Remove RequestSupport

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/requestsupport/IRequestSupportView.java
+++
@@ -1,9 +0,0 @@
-package vn.com.vng.zalopay.requestsupport;
-
-import android.net.Uri;
-
-import vn.com.vng.zalopay.ui.view.ILoadDataView;
-
-public interface IRequestSupportView extends ILoadDataView {
-    void insertScreenshot(Uri uri);
-}
