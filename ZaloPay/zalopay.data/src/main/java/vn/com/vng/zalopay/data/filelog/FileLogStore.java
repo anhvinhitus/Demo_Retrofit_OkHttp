36e3be0b7730d52ed45d2cb8f7e9843f13e2334a
khattn <khattn2@vng.com.vn>
2017-05-08 11:42:18 +0700

    [App] Missing timing event for API `v001/zp-upload/clientlogs` (tasks #475)

--- ZaloPay/zalopay.data/src/main/java/vn/com/vng/zalopay/data/filelog/FileLogStore.java
+++ ZaloPay/zalopay.data/src/main/java/vn/com/vng/zalopay/data/filelog/FileLogStore.java
@@ -8,6 +8,8 @@ import retrofit2.http.Part;
 import rx.Observable;
 import vn.com.vng.zalopay.data.Constants;
 import vn.com.vng.zalopay.data.api.response.BaseResponse;
+import vn.com.vng.zalopay.network.API_NAME;
+import vn.com.zalopay.analytics.ZPEvents;
 
 /**
  * Created by hieuvm on 4/21/17.
@@ -18,6 +20,7 @@ public interface FileLogStore {
 
     interface RequestService {
         @Multipart
+        @API_NAME(ZPEvents.API_V001_ZP_UPLOAD_CLIENTLOGS)
         @POST(Constants.UMUPLOAD_API.FILE_LOG)
         Observable<BaseResponse> uploadFileLog(@Part("userid") RequestBody userid, @Part MultipartBody.Part file);
     }
