57d12a8eeb8ee6f17c9a67a3f2cac44df76287b4
hieuvm <hieuvm@vng.com.vn>
2017-05-12 09:55:07 +0700

    [ZPAnalytics] Add Apptransid Tracker

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/tracker/ZPTrackerApptransid.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/tracker/ZPTrackerApptransid.java
@@ -1,23 +1,22 @@
 package vn.com.vng.zalopay.tracker;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import vn.com.vng.zalopay.data.api.entity.ApptransidLogEntity;
+import rx.schedulers.Schedulers;
 import vn.com.vng.zalopay.data.apptransidlog.ApptransidLogStore;
-import vn.com.vng.zalopay.utils.TrackBuilders;
+import vn.com.vng.zalopay.domain.interactor.DefaultSubscriber;
+import vn.com.zalopay.analytics.ZPApptransidLog;
 import vn.com.zalopay.analytics.ZPTracker;
 
 /**
  * Created by khattn on 1/23/17.
+ * ZPTracker apptransid
  */
 
 public class ZPTrackerApptransid implements ZPTracker {
 
     private ApptransidLogStore.Repository mRepository;
 
-    public ZPTrackerApptransid(ApptransidLogStore.Repository logLocalStorage) {
-        this.mRepository = logLocalStorage;
+    public ZPTrackerApptransid(ApptransidLogStore.Repository repository) {
+        this.mRepository = repository;
     }
 
     @Override
@@ -36,56 +35,14 @@ public class ZPTrackerApptransid implements ZPTracker {
     }
 
     @Override
-    public void trackApptransidEvent(String apptransid, int appid, int step, int step_result,
-                                     int pcmid, int transtype, long transid, int sdk_result,
-                                     int server_result, String source) {
-
-        final TrackBuilders.AppTransIdBuilder eventBuilder = new TrackBuilders.AppTransIdBuilder()
-                .setAppTransId(apptransid)
-                .setAppId(appid)
-                .setStep(step)
-                .setStepResult(step_result)
-                .setPcmId(pcmid)
-                .setTransType(transtype)
-                .setTransId(transid)
-                .setSdkResult(sdk_result)
-                .setServerResult(server_result)
-                .setSource(source);
-
-        mRepository.remove(apptransid).subscribe();
-//        mRepository.put(eventBuilder.build()).subscribe();
-//        mRepository.get(apptransid).subscribe(new Observer<ApptransidLogEntity>() {
-//            @Override
-//            public void onCompleted() {
-//
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//
-//            }
-//
-//            @Override
-//            public void onNext(ApptransidLogEntity apptransidLog) {
-//                Log.d("TEST", "trackLog: " + transform(apptransidLog).toString());
-//            }
-//        });
+    public void trackApptransidEvent(ZPApptransidLog log) {
+        mRepository.put(log)
+                .subscribeOn(Schedulers.io())
+                .subscribe(new DefaultSubscriber<>());
     }
 
-    private static Map<String, String> transform(ApptransidLogEntity data) {
-        Map<String, String> log = new HashMap<>();
+    @Override
+    public void trackAPIError(String apiName, int httpCode, int serverCode, int networkCode) {
 
-        log.put("apptransid", data.apptransid);
-        log.put("appid", String.valueOf(data.appid));
-        log.put("step", String.valueOf(data.step));
-        log.put("step_result", String.valueOf(data.step_result));
-        log.put("pcmid", String.valueOf(data.pcmid));
-        log.put("transtype", String.valueOf(data.transtype));
-        log.put("transid", String.valueOf(data.transid));
-        log.put("sdk_result", String.valueOf(data.sdk_result));
-        log.put("server_result", String.valueOf(data.server_result));
-        log.put("source", data.source);
-
-        return log;
     }
 }
