d25732b2536d5e2eb7da3381ea4aa47ca548086d
hieuvm <hieuvm@vng.com.vn>
2017-05-18 09:52:45 +0700

    [FileLog] Update url file log - Còn thiểu url của production

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/internal/di/modules/UserFileLogModule.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/internal/di/modules/UserFileLogModule.java
@@ -5,6 +5,7 @@ import javax.inject.Named;
 import dagger.Module;
 import dagger.Provides;
 import retrofit2.Retrofit;
+import vn.com.vng.zalopay.BuildConfig;
 import vn.com.vng.zalopay.data.filelog.FileLogRepository;
 import vn.com.vng.zalopay.data.filelog.FileLogStore;
 import vn.com.vng.zalopay.domain.model.User;
@@ -21,7 +22,7 @@ public class UserFileLogModule {
     @UserScope
     @Provides
     FileLogStore.Repository providesFileLogRepository(User user, @Named("retrofitPhoto") Retrofit retrofit) {
-        return new FileLogRepository(user, retrofit.create(FileLogStore.RequestService.class));
+        return new FileLogRepository(user, BuildConfig.CLIENTLOGS_URL, retrofit.create(FileLogStore.RequestService.class));
     }
 
 }
--- ZaloPay/zalopay.data/src/main/java/vn/com/vng/zalopay/data/filelog/FileLogStore.java
+++ ZaloPay/zalopay.data/src/main/java/vn/com/vng/zalopay/data/filelog/FileLogStore.java
@@ -5,6 +5,7 @@ import okhttp3.RequestBody;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Url;
 import rx.Observable;
 import vn.com.vng.zalopay.data.Constants;
 import vn.com.vng.zalopay.data.api.response.BaseResponse;
@@ -19,7 +20,7 @@ public interface FileLogStore {
     interface RequestService {
         @Multipart
         @POST(Constants.UMUPLOAD_API.FILE_LOG)
-        Observable<BaseResponse> uploadFileLog(@Part("userid") RequestBody userid, @Part MultipartBody.Part file);
+        Observable<BaseResponse> uploadFileLog(@Url String url, @Part("userid") RequestBody userid, @Part MultipartBody.Part file);
     }
 
     interface Repository {
