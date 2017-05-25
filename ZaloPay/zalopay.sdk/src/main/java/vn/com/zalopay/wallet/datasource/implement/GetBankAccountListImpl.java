854421d1b6cec391c37e12d886d28157a42ec054
hieuvm <hieuvm@vng.com.vn>
2017-05-25 11:36:00 +0700

    [Analytics] Add timing api get bank account

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/datasource/implement/GetBankAccountListImpl.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/datasource/implement/GetBankAccountListImpl.java
@@ -3,6 +3,7 @@ package vn.com.zalopay.wallet.datasource.implement;
 import java.util.HashMap;
 
 import retrofit2.Call;
+import vn.com.zalopay.analytics.ZPEvents;
 import vn.com.zalopay.wallet.datasource.IData;
 import vn.com.zalopay.wallet.datasource.interfaces.ITask;
 
@@ -14,6 +15,6 @@ public class GetBankAccountListImpl implements ITask {
 
     @Override
     public int getTaskEventId() {
-        return 0;
+        return ZPEvents.API_UM_LISTBANKACCOUNTFORCLIENT;
     }
 }
