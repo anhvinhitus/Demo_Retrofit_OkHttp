7bce9db9daca662b7f24362868357e7b89e7b35d
hieuvm <hieuvm@vng.com.vn>
2017-05-25 11:51:44 +0700

    [Analytics] Add timing api get bank account

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/datasource/IData.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/datasource/IData.java
@@ -80,7 +80,7 @@ public interface IData {
      * @return
      */
     @GET(Constants.URL_LISTBANKACCCOUNT)
-    // @TODO
+    @API_NAME(ZPEvents.API_UM_LISTBANKACCOUNTFORCLIENT)
     Observable<BankAccountListResponse> loadMapBankAccountList(@QueryMap Map<String, String> params);
 
     /***
