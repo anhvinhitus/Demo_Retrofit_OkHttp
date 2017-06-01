794229a4636339957ca784f64c4b2c1727d28708
hieuvm <hieuvm@vng.com.vn>
2017-06-01 12:37:24 +0700

    [SmsReceiver] Fix NPE SmsMessageBase

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/receivers/SmsReceiver.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/receivers/SmsReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.text.TextUtils;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -23,36 +24,56 @@ public class SmsReceiver extends BroadcastReceiver {
         try {
             //---get the SMS message passed in---
             Bundle bundle = intent.getExtras();
+
+            if (bundle == null) {
+                return;
+            }
+
             SmsMessage[] msgs = null;
             String str = "";
-            if (bundle != null) {
 
-                ReceiveSmsEvent event = new ReceiveSmsEvent();
-                //---retrieve the SMS message received---
-                Object[] pdus = (Object[]) bundle.get("pdus");
-                if (pdus != null) {
-                    msgs = new SmsMessage[pdus.length];
-                }
-                if (msgs != null) {
-                    for (int i = 0; i < msgs.length; i++) {
-                        msgs[i] = createFromPdu((byte[]) pdus[i], bundle);
-                        if (msgs[i] == null) {
-                            continue;
-                        }
-                        str += "SMS from " + msgs[i].getOriginatingAddress();
-                        str += " :";
-                        str += msgs[i].getMessageBody();
-                        event.addMessage(msgs[i].getOriginatingAddress(), msgs[i].getMessageBody());
-                        str += "\n";
-                    }
-                }
-                EventBus eventBus = AndroidApplication.instance().getAppComponent().eventBus();
-                eventBus.removeStickyEvent(ReceiveSmsEvent.class);
-                eventBus.postSticky(event);
-
-                //---display the new SMS message---
-                Timber.d(str);
+            ReceiveSmsEvent event = new ReceiveSmsEvent();
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            if (pdus != null) {
+                msgs = new SmsMessage[pdus.length];
             }
+
+            if (msgs != null) {
+                for (int i = 0; i < msgs.length; i++) {
+                    SmsMessage smsMessage = createFromPdu((byte[]) pdus[i], bundle);
+                    if (smsMessage == null) {
+                        continue;
+                    }
+
+                    String origAddress = null;
+                    String messageBody = null;
+
+                    try {
+                        origAddress = smsMessage.getOriginatingAddress();
+                        messageBody = smsMessage.getMessageBody();
+                    } catch (NullPointerException ignore) {
+                    }
+
+                    if (TextUtils.isEmpty(origAddress) && TextUtils.isEmpty(messageBody)) {
+                        continue;
+                    }
+
+                    str += "SMS from " + origAddress;
+                    str += " :";
+                    str += messageBody;
+                    event.addMessage(origAddress, messageBody);
+                    str += "\n";
+                }
+            }
+            EventBus eventBus = AndroidApplication.instance().getAppComponent().eventBus();
+            eventBus.removeStickyEvent(ReceiveSmsEvent.class);
+            eventBus.postSticky(event);
+
+            //---display the new SMS message---
+            Timber.d(str);
+
+
         } catch (Throwable t) {
             Timber.w(t, "Got exception while processing received sms");
         }
