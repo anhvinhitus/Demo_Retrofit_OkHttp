26486673f95d7be79d7444d2c4c03acd2a9128ec
lytm <lytm@vng.com.vn>
2017-06-19 17:39:50 +0700

    [WebView] Refactor AgentUtil

--- ZaloPay/ui.widget/src/main/java/com/zalopay/ui/widget/util/AgentUtil.java
+++ ZaloPay/ui.widget/src/main/java/com/zalopay/ui/widget/util/AgentUtil.java
@@ -20,12 +20,6 @@ public class AgentUtil {
     }
 
     public static String getUserAgent(String userAgent) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            return "Mozilla/5.0 (Linux; Android 5.1.1; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" + " " + userAgent;
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            return "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" + " " + userAgent;
-        } else {
-            return "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" + " " + userAgent;
-        }
+        return getUserAgent() + userAgent;
     }
 }
