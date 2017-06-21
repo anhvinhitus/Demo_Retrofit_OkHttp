1fa466a9e3abe2c5ac563489d77a51e7d2975f55
lytm <lytm@vng.com.vn>
2017-06-21 17:54:35 +0700

    [APP] Update UI Search View

---
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/ui/widget/GridSpacingItemSearchDecoration.java
@@ -0,0 +1,38 @@
+package vn.com.vng.zalopay.ui.widget;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by AnhHieu on 5/25/16.
+ */
+public class GridSpacingItemSearchDecoration extends RecyclerView.ItemDecoration {
+    private int spanCount;
+    private int spacing;
+    private boolean includeEdge;
+
+    public GridSpacingItemSearchDecoration(int spanCount, int spacing, boolean includeEdge) {
+        this.spanCount = spanCount;
+        this.spacing = spacing;
+        this.includeEdge = includeEdge;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        int position = parent.getChildAdapterPosition(view); // item position
+        int column = position % spanCount; // item column
+
+        if (includeEdge) {
+            outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+            outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+            if (position < spanCount) { // top edge
+                outRect.top = spacing;
+            }
+            outRect.bottom = spacing; // item bottom
+        } else {
+            outRect.bottom = spacing;
+        }
+    }
+}
