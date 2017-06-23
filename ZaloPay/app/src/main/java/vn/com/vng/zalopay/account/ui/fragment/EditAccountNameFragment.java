faba59a87d6b927b8d05e6876767de2e416a987b
hieuvm <hieuvm@vng.com.vn>
2017-06-23 14:12:36 +0700

    [AccountName] Task#620: Bug UI build 277
    - Sửa lỗi bottom bị bàn phím che mất

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/account/ui/fragment/EditAccountNameFragment.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/account/ui/fragment/EditAccountNameFragment.java
@@ -68,12 +68,7 @@ public class EditAccountNameFragment extends BaseFragment implements IEditAccoun
 
         mAccountNameView.addValidator(new MinCharactersValidate(getString(R.string.exception_account_name_length), 4));
         mAccountNameView.addValidator(new SpecialCharactersValidate(getString(R.string.exception_account_name_special_char)));
-        mAccountNameView.setClearTextListener(new ZPEditText.OnClearTextListener() {
-            @Override
-            public void onClearTextSuccess() {
-                ZPAnalytics.trackEvent(ZPEvents.UPDATEZPN_EDIT_DELETE);
-            }
-        });
+        mAccountNameView.setClearTextListener(() -> ZPAnalytics.trackEvent(ZPEvents.UPDATEZPN_EDIT_DELETE));
     }
 
     @OnTextChanged(value = R.id.edtAccountName, callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
