39997090489a4433bc02cdc69557c48ec1181256
vo van chuc <chucvv@vng.com.vn>
2017-06-23 15:26:06 +0700

    [SDK] Thêm redirect_url vào appinfo

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/gatewayinfo/AppInfo.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/business/entity/gatewayinfo/AppInfo.java
@@ -1,15 +1,21 @@
 package vn.com.zalopay.wallet.business.entity.gatewayinfo;
 
 
+import com.google.gson.annotations.SerializedName;
+
 public class AppInfo {
-    public String appname = null;
+    @SerializedName("appname")
+    public String appname;
+    @SerializedName("appid")
     public long appid = Long.MIN_VALUE;
+    @SerializedName("status")
     public int status = -1;
-    public int viewresulttype = 1;
+    @SerializedName("expriretime")
+    public long expriretime;
+    @SerializedName("redirect_url")
+    public String redirect_url;
 
     public boolean isAllow() {
         return status == 1;
     }
-
-    public long expriretime;
 }
