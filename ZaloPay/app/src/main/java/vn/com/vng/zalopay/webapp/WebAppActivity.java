a64134a1c884b82e0d21207e6183747efc3b06bf
datnt10 <datnt10@vng.com.vn>
2017-06-30 10:38:28 +0700

    [WebApp] task#650 [WebApp] Add Close button on Navigation bar

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppActivity.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppActivity.java
@@ -1,9 +1,13 @@
 package vn.com.vng.zalopay.webapp;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.TextView;
 
-import com.zalopay.ui.widget.IconFontDrawable;
+import com.zalopay.ui.widget.IconFont;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import vn.com.vng.zalopay.R;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
 import vn.com.vng.zalopay.user.UserBaseToolBarActivity;
@@ -12,6 +16,25 @@ public class WebAppActivity extends UserBaseToolBarActivity {
     @Override
     public BaseFragment getFragmentToHost() {
         return WebAppFragment.newInstance(getIntent().getExtras());
+    }
+
+    @BindView(R.id.title)
+    TextView tvTitle;
+
+    @BindView(R.id.btn_back)
+    IconFont btnBack;
+
+    @BindView(R.id.btn_close)
+    IconFont btnClose;
+
+    @OnClick(R.id.close_view)
+    void onCloseWebAppClicked() {
+        finish();
+    }
+
+    @OnClick(R.id.back_view)
+    void onBackWebAppClicked() {
+        onBackPressed();
     }
 
     @Override
@@ -23,11 +46,29 @@ public class WebAppActivity extends UserBaseToolBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getToolbar().setNavigationIcon(
-                new IconFontDrawable(this)
-                        .setIcon(R.string.general_backandroid)
-                        .setResourcesColor(R.color.colorWebAppPrimaryText)
-                        .setDpSize(18)
-        );
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        btnBack.setIcon(R.string.general_backandroid);
+        btnBack.setIconColor(R.color.colorWebAppPrimaryText);
+        btnBack.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
+
+        btnClose.setIcon(R.string.webapp_ico_delete);
+        btnClose.setIconColor(R.color.colorWebAppPrimaryText);
+        btnClose.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
+
+//        getToolbar().setNavigationIcon(
+//                new IconFontDrawable(this)
+//                        .setIcon(R.string.general_backandroid)
+//                        .setResourcesColor(R.color.colorWebAppPrimaryText)
+//                        .setDpSize(18)
+//        );
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        tvTitle.setText(title);
     }
 }
