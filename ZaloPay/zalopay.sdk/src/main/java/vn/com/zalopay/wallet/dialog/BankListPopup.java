56887c9c7414ddb0ee7c48a696dcf6ea1edd5997
vo van chuc <chucvv@vng.com.vn>
2017-07-06 11:20:39 +0700

    [Bank] #ZPF-249 : [Android - 2.14 - Staging] thiếu ngân hàng hổ trợ : Sửa title thành Các Ngân hàng được hỗ trợ

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/dialog/BankListPopup.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/dialog/BankListPopup.java
@@ -1,9 +1,7 @@
 package vn.com.zalopay.wallet.dialog;
 
-import android.text.Html;
 import android.view.View;
 import android.widget.GridView;
-import android.widget.TextView;
 
 import com.zalopay.ui.widget.dialog.listener.ZPWOnCloseDialogListener;
 
@@ -11,18 +9,13 @@ import java.lang.ref.WeakReference;
 
 import timber.log.Timber;
 import vn.com.zalopay.wallet.R;
-import vn.com.zalopay.wallet.business.data.GlobalData;
-import vn.com.zalopay.wallet.business.data.RS;
-import vn.com.zalopay.wallet.constants.TransactionType;
 import vn.com.zalopay.wallet.view.adapter.CardSupportAdapter;
-import vn.com.zalopay.wallet.view.adapter.CreditCardSupportGridViewAdapter;
 
 public class BankListPopup extends BasePaymentDialogActivity {
 
     protected static WeakReference<ZPWOnCloseDialogListener> mCloseCardSupportDialogListener;
     protected static WeakReference<CardSupportAdapter> mCardSupportGridViewAdapter;
     protected GridView mGridViewBank;
-    protected TextView txtLabel;
     protected View mRippleButtonSelectBank;
 
     public static void setCloseDialogListener(ZPWOnCloseDialogListener pListener) {
@@ -63,7 +56,6 @@ public class BankListPopup extends BasePaymentDialogActivity {
     @Override
     protected void initViews() {
         mGridViewBank = (GridView) findViewById(R.id.gridViewCard);
-        txtLabel = (TextView) findViewById(R.id.cardsupport_label);
         mRippleButtonSelectBank = findViewById(R.id.rippleButtonSelectBank);
         mRippleButtonSelectBank.setOnClickListener(view -> onBackPressed());
     }
@@ -73,14 +65,6 @@ public class BankListPopup extends BasePaymentDialogActivity {
         try {
             if (getAdapter() != null) {
                 mGridViewBank.setAdapter(getAdapter());
-            }
-
-            if (GlobalData.transtype() == TransactionType.LINK) {
-                txtLabel.setText(Html.fromHtml(GlobalData.getStringResource(RS.string.zpw_string_title_select_card)));
-            } else if (getAdapter() instanceof CreditCardSupportGridViewAdapter) {
-                txtLabel.setText(Html.fromHtml(GlobalData.getStringResource(RS.string.zpw_string_title_select_card)));
-            } else {
-                txtLabel.setText(GlobalData.getStringResource(RS.string.zpw_string_title_select_bank));
             }
         } catch (Exception e) {
             Timber.d("get card support Adapter: [%s]", e);
