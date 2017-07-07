cb081390e14b4e28354126c863079dc16b00193e
lytm <lytm@vng.com.vn>
2017-07-07 11:46:41 +0700

    [Password] Update show Password in UI thread

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/navigation/Navigator.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/navigation/Navigator.java
@@ -7,7 +7,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -846,8 +849,16 @@ public class Navigator implements INavigator {
             Helpers.promiseResolveSuccess(promise, null);
             return true;
         }
-
-        showPinDialog(context, promise);
+        boolean isUiThread = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? Looper.getMainLooper().isCurrentThread() : Thread.currentThread() == Looper.getMainLooper().getThread();
+        if (!isUiThread) {
+            Timber.d("notification promotion event coming from background, switching thread to main thread...");
+            new Handler(Looper.getMainLooper()).post(() -> {
+                //this runs on the UI thread
+                showPinDialog(context, promise);
+            });
+        } else {
+            showPinDialog(context, promise);
+        }
         return false;
     }
 
