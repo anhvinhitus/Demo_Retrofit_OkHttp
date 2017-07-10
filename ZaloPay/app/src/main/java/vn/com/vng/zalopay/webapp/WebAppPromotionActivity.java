759a1a217cc87ffef8f6e2d37bd3158a433e29e9
hieuvm <hieuvm@vng.com.vn>
2017-07-10 11:06:38 +0700

    [WebApp] Sử dụng user activity

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppPromotionActivity.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/WebAppPromotionActivity.java
@@ -1,10 +1,10 @@
 package vn.com.vng.zalopay.webapp;
 
 import vn.com.vng.zalopay.R;
-import vn.com.vng.zalopay.ui.activity.BaseToolBarActivity;
 import vn.com.vng.zalopay.ui.fragment.BaseFragment;
+import vn.com.vng.zalopay.user.UserBaseToolBarActivity;
 
-public class WebAppPromotionActivity extends BaseToolBarActivity {
+public class WebAppPromotionActivity extends UserBaseToolBarActivity {
     @Override
     public BaseFragment getFragmentToHost() {
         return WebAppPromotionFragment.newInstance(getIntent().getExtras());
