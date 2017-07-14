7e444e9f6dd7f5b6be4f770da65fcd29f400c579
hieuvm <hieuvm@vng.com.vn>
2017-07-14 10:40:46 +0700

    [UnitTest] Update unittest lists

--- ZaloPay/zalopay.data/src/test/java/vn/com/vng/zalopay/data/util/ListsTest.java
+++ ZaloPay/zalopay.data/src/test/java/vn/com/vng/zalopay/data/util/ListsTest.java
@@ -47,4 +47,24 @@ public class ListsTest {
         System.out.println(result.toString());
     }
 
+    @Test
+    public void choppedCheckElementInteger() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        List<List<Integer>> result = Lists.chopped(numbers, 4);
+        Assert.assertTrue(result.get(0).toString().equals(numbers.subList(0, 4).toString()));
+        Assert.assertTrue(result.get(1).toString().equals(numbers.subList(4, 8).toString()));
+
+        result = Lists.chopped(numbers, 3);
+        Assert.assertTrue(result.get(0).toString().equals(numbers.subList(0, 3).toString()));
+        Assert.assertTrue(result.get(1).toString().equals(numbers.subList(3, 6).toString()));
+        Assert.assertTrue(result.get(2).toString().equals(numbers.subList(6, 8).toString()));
+
+        result = Lists.chopped(numbers, 2);
+        Assert.assertTrue(result.get(0).toString().equals(numbers.subList(0, 2).toString()));
+        Assert.assertTrue(result.get(1).toString().equals(numbers.subList(2, 4).toString()));
+        Assert.assertTrue(result.get(2).toString().equals(numbers.subList(4, 6).toString()));
+        Assert.assertTrue(result.get(3).toString().equals(numbers.subList(6, 8).toString()));
+    }
+
 }
