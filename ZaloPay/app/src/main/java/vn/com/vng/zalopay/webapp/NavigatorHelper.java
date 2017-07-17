eb41d5dd85ec098f6e59fe4921b1226720b8d34c
sinhtt <sinhtt@vng.com.vn>
2017-07-17 11:55:21 +0700

    [SharePromotion] show alert "Bạn chưa cài đặt ứng dụng Zalo" when Zalo app is not install

--- ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/NavigatorHelper.java
+++ ZaloPay/app/src/main/java/vn/com/vng/zalopay/webapp/NavigatorHelper.java
@@ -1,5 +1,6 @@
 package vn.com.vng.zalopay.webapp;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import timber.log.Timber;
+import vn.com.vng.zalopay.R;
+import vn.com.vng.zalopay.utils.DialogHelper;
 
 /**
  * Created by huuhoa on 5/16/17.
@@ -25,25 +28,34 @@ class NavigatorHelper {
             shareIntent.setType("text/plain");
             List<ResolveInfo> resolveInfos = context.getPackageManager().queryIntentActivities(shareIntent, 0);
 
-            if (!resolveInfos.isEmpty()) {
-                for (ResolveInfo resolveInfo : resolveInfos) {
-                    String packageName = resolveInfo.activityInfo.packageName;
-                    if (packageName.contains("zalo")) {
-                        Intent intent = new Intent();
-                        intent.setComponent(new ComponentName(packageName, resolveInfo.activityInfo.name));
-                        intent.setAction(Intent.ACTION_SEND);
-                        intent.setType("text/plain");
-                        intent.putExtra(Intent.EXTRA_TEXT, currentUrl);
-                        intent.setPackage(packageName);
-                        targetShareIntents.add(intent);
-                    }
-                }
+            if (resolveInfos.isEmpty())
+                return;
 
-                if (!targetShareIntents.isEmpty()) {
-                    Intent chooserIntent = Intent.createChooser(targetShareIntents.get(0), "Choose app to share");
-                    context.startActivity(chooserIntent);
+            // ////////////////////////////////////////
+            // FIND ZALO APP IN LIST APP FOR SHARING //
+            // ////////////////////////////////////////
+            for (ResolveInfo resolveInfo : resolveInfos) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                if (packageName.contains("zalo")) { // find zalo app
+                    Intent intent = new Intent();
+                    intent.setComponent(new ComponentName(packageName, resolveInfo.activityInfo.name));
+                    intent.setAction(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.putExtra(Intent.EXTRA_TEXT, currentUrl);
+                    intent.setPackage(packageName);
+                    targetShareIntents.add(intent);
                 }
             }
+
+            if (targetShareIntents.isEmpty()) {
+                DialogHelper.showNotificationDialog((Activity) context, context.getString(R.string.alert_not_exist_zalo));
+                return;
+            }
+
+            // show chooser for choosing app to share
+            Intent chooserIntent = Intent.createChooser(targetShareIntents.get(0), "Choose app to share");
+            context.startActivity(chooserIntent);
+
         } catch (Exception e) {
             Timber.e(e, "Cannot share web on Zalo");
         }
