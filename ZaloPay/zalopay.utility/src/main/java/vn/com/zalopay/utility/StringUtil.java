0832158d4b025063a38da5221a684bf410a2494f
vo van chuc <chucvv@vng.com.vn>
2017-07-22 13:19:51 +0700

    [Utils] Cleanup

--- ZaloPay/zalopay.utility/src/main/java/vn/com/zalopay/utility/StringUtil.java
+++ ZaloPay/zalopay.utility/src/main/java/vn/com/zalopay/utility/StringUtil.java
@@ -2,43 +2,18 @@ package vn.com.zalopay.utility;
 
 import android.text.TextUtils;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Currency;
-import java.util.Locale;
-
 public class StringUtil {
-
-    public static String formatVnCurrence(String price) {
-
-        NumberFormat format = new DecimalFormat("#,##0.00");
-
-        format.setCurrency(Currency.getInstance(Locale.US));//Or default locale
-
-        price = (!TextUtils.isEmpty(price)) ? price : "0";
-
-        price = price.trim();
-        price = format.format(Math.ceil(Double.parseDouble(price)));
-
-        price = price.replaceAll(",", "\\.");
-
-        if (price.endsWith(".00")) {
-            int centsIndex = price.lastIndexOf(".00");
-            if (centsIndex != -1) {
-                price = price.substring(0, centsIndex);
-            }
-        }
-
-        price = String.format("%s", price);
-
-        return price;
-    }
-
     public static String getFirstStringWithSize(String pString, int pSize) {
+        if (TextUtils.isEmpty(pString) || pString.length() <= pSize) {
+            return "";
+        }
         return pString.substring(0, pSize);
     }
 
     public static String getLastStringWithSize(String pString, int pSize) {
+        if (TextUtils.isEmpty(pString)) {
+            return "";
+        }
         return pString.substring(pString.length() - pSize);
     }
 }
