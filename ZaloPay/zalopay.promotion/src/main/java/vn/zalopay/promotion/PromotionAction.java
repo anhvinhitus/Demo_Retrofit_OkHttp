87c91493a22c73b26b18c57c31fa636bd02e5f7a
vo van chuc <chucvv@vng.com.vn>
2017-07-25 09:02:26 +0700

    [Promotion] Add SerializedName để parse object

--- ZaloPay/zalopay.promotion/src/main/java/vn/zalopay/promotion/PromotionAction.java
+++ ZaloPay/zalopay.promotion/src/main/java/vn/zalopay/promotion/PromotionAction.java
@@ -1,13 +1,12 @@
 package vn.zalopay.promotion;
 
+import com.google.gson.annotations.SerializedName;
+
 public class PromotionAction {
+    @SerializedName("title")
     public String title;
 
+    @SerializedName("action")
     @ActionType
     public int action;
-
-    public PromotionAction(String pTitle, int pAction) {
-        title = pTitle;
-        action = pAction;
-    }
 }
--- ZaloPay/zalopay.promotion/src/main/java/vn/zalopay/promotion/PromotionEvent.java
+++ ZaloPay/zalopay.promotion/src/main/java/vn/zalopay/promotion/PromotionEvent.java
@@ -1,28 +1,30 @@
 package vn.zalopay.promotion;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
-import vn.zalopay.promotion.PromotionAction;
-import vn.zalopay.promotion.PromotionType;
-
 public class PromotionEvent {
+    @SerializedName("type")
     @PromotionType
     public int type;
 
+    @SerializedName("title")
     public String title;
-    public long amount;
-    public String campaign;
-    public List<PromotionAction> actions;
-    public long transid;
-    public long notificationId;
 
-    public PromotionEvent(int pType, String pTitle, long pAmount, String pCampain, List<PromotionAction> pAction, long pTransId, long pNotificationId) {
-        type = pType;
-        title = pTitle;
-        amount = pAmount;
-        campaign = pCampain;
-        actions = pAction;
-        transid = pTransId;
-        notificationId = pNotificationId;
-    }
+    @SerializedName("amount")
+    public long amount;
+
+    @SerializedName("campaign")
+    public String campaign;
+
+    @SerializedName("actions")
+    public List<PromotionAction> actions;
+
+    @Expose
+    public long transid;
+
+    @Expose
+    public long notificationId;
 }
