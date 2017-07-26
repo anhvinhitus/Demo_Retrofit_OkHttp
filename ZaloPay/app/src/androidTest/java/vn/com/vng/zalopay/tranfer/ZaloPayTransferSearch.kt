8533965098bfa6f0173a3c6d9c5f773c7bce3cf9
sinhtt <sinhtt@vng.com.vn>
2017-07-26 18:07:58 +0700

    [Testcase] search & process transfer

---
+++ ZaloPay/app/src/androidTest/java/vn/com/vng/zalopay/tranfer/ZaloPayTransferSearch.kt
@@ -0,0 +1,27 @@
+package vn.com.vng.zalopay.tranfer
+
+import android.support.test.espresso.matcher.ViewMatchers
+import org.junit.Test
+import vn.com.vng.zalopay.AbtractZaloPayTesting
+import vn.com.vng.zalopay.R
+import vn.com.vng.zalopay.sb.EZaloPayTransfer
+import vn.com.vng.zalopay.sb.Info
+
+/**
+ * Created by cpu11843-local on 7/26/17.
+ */
+class ZaloPayTransferSearch : AbtractZaloPayTesting(), IZaloPayTransfer {
+    override fun initTest() {
+        super.initTest()
+    }
+
+    @Test
+    fun transfer_100k_with_search_HAPPYCASE() {
+        processInApp(EZaloPayTransfer.TRANSFER_SEARCH, java.lang.Long.parseLong(getText(ViewMatchers.withId(R.id.tv_balance)).replace(".", "")), Info.AMOUNT_100K)
+    }
+
+    @Test
+    fun transfer_verify_near_list_transaction_HAPPYCASE() {
+        processInApp(EZaloPayTransfer.TRANSFER_VERIFY_LIST_NEAR_TRANS, 0, 0)
+    }
+}
