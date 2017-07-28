e3ce93148cdba36f3d733c147f43222423d8e595
datnt10 <datnt10@vng.com.vn>
2017-07-28 15:57:30 +0700

    [App] Home: add more properties in config "internal_apps"

--- ZaloPay/zalopay.domain/src/main/java/vn/com/vng/zalopay/domain/model/InternalApp.java
+++ ZaloPay/zalopay.domain/src/main/java/vn/com/vng/zalopay/domain/model/InternalApp.java
@@ -13,6 +13,15 @@ public class InternalApp {
     @SerializedName("order")
     private int position;
 
+    @SerializedName("display_name")
+    private String displayName;
+
+    @SerializedName("icon_name")
+    private String iconName;
+
+    @SerializedName("icon_color")
+    private String iconColor;
+
     public long getAppId() {
         return appId;
     }
@@ -29,12 +38,39 @@ public class InternalApp {
         this.position = position;
     }
 
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getIconName() {
+        return iconName;
+    }
+
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+
+    public String getIconColor() {
+        return iconColor;
+    }
+
+    public void setIconColor(String iconColor) {
+        this.iconColor = iconColor;
+    }
+
     public InternalApp() {
 
     }
 
-    public InternalApp(long appId, int position) {
+    public InternalApp(long appId, int position, String displayName, String iconName, String iconColor) {
         this.appId = appId;
         this.position = position;
+        this.displayName = displayName;
+        this.iconName = iconName;
+        this.iconColor = iconColor;
     }
 }
