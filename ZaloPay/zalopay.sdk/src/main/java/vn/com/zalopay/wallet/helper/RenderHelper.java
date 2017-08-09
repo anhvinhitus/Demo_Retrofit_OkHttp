fb231876aed17f68acbef3c5fc10cdf91ce7b52a
chucvv <chucvv@vng.com.vn>
2017-08-09 12:57:09 +0700

    Revert "[SDK] CPS-72: Fix lỗi màn hình kết quả tên dài bị bể layout"
    This reverts commit 49b3eeb7d1c1666a9afa1566379abaed78d5e2bc.

--- ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/helper/RenderHelper.java
+++ ZaloPay/zalopay.sdk/src/main/java/vn/com/zalopay/wallet/helper/RenderHelper.java
@@ -8,9 +8,11 @@ import android.support.v4.content.ContextCompat;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.lang.reflect.Field;
@@ -40,27 +42,38 @@ public class RenderHelper {
             return null;
         }
         List<View> views = new ArrayList<>();
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View pLayout = inflater.inflate(R.layout.layout_info, null, false);
         for (int i = 0; i < nameValuePairList.size(); i++) {
             NameValuePair nameValuePair = nameValuePairList.get(i);
-            if (nameValuePair == null) {
+            if(nameValuePair == null){
                 continue;
             }
-            if (pLayout == null) {
-                return null;
-            }
+            RelativeLayout relativeLayout = new RelativeLayout(context);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.bottomMargin = (int) context.getResources().getDimension(R.dimen.sdk_margin_left_right);
+            relativeLayout.setLayoutParams(params);
             if (!TextUtils.isEmpty(nameValuePair.key)) {
-                TextView name_txt = (TextView) pLayout.findViewById(R.id.key_txt);
+                TextView name_txt = new TextView(context);
                 name_txt.setTextColor(ContextCompat.getColor(context, (R.color.text_color_grey)));
                 name_txt.setText(nameValuePair.key);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                layoutParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                name_txt.setLayoutParams(layoutParams);
+                relativeLayout.addView(name_txt);
             }
             if (!TextUtils.isEmpty(nameValuePair.value)) {
-                TextView value_txt = (TextView) pLayout.findViewById(R.id.value_txt);
+                TextView value_txt = new TextView(context);
                 value_txt.setTextColor(ContextCompat.getColor(context, (R.color.text_color_grey)));
                 value_txt.setText(nameValuePair.value);
+                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                layoutParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                value_txt.setLayoutParams(layoutParams);
+                relativeLayout.addView(value_txt);
             }
-            views.add(pLayout);
+            views.add(relativeLayout);
         }
         return views;
     }
@@ -69,13 +82,13 @@ public class RenderHelper {
         if (pEditext == null) {
             return;
         }
-        if ((!(pEditext instanceof VPaymentEditText) || ((VPaymentEditText) pEditext).getTextInputLayout() == null)) {
+        if(( !(pEditext instanceof VPaymentEditText) || ((VPaymentEditText) pEditext).getTextInputLayout() == null)){
             return;
         }
 
         try {
             TextInputLayout textInputLayout = ((VPaymentEditText) pEditext).getTextInputLayout();
-            if (textInputLayout == null) {
+            if(textInputLayout == null){
                 return;
             }
 
@@ -108,12 +121,12 @@ public class RenderHelper {
         if (pEditext == null) {
             return;
         }
-        if ((!(pEditext instanceof VPaymentEditText) || ((VPaymentEditText) pEditext).getTextInputLayout() == null)) {
+        if(( !(pEditext instanceof VPaymentEditText) || ((VPaymentEditText) pEditext).getTextInputLayout() == null)){
             return;
         }
         try {
             TextInputLayout textInputLayout = ((VPaymentEditText) pEditext).getTextInputLayout();
-            if (textInputLayout == null) {
+            if(textInputLayout == null){
                 return;
             }
             int color = pContext.getResources().getColor(R.color.holo_red_light);
